c1ccdcf98bc1fabf91d41b7608cd524d7c3bd629
sohamch <ch.soham@gmail.com>
2020-01-30 13:12:19 -0600

    put spins in separate function, since need to use only once.

--- Cluster_basis.py
+++ Cluster_basis.py
@@ -1,8 +1,19 @@
 from onsager import crystal, cluster, supercell
 import numpy as np
 
+def createSpins(mobile_count, spec_count=0):
 
-def createLbam(sup, clusexp, mobile_occ, spec_count=0):
+    m = (len(mobile_occ) + spec_count) // 2
+    # The number of species in 2m+1 or 2m.
+
+    spins = list(range(-m, m + 1))
+
+    if m % 2 == 1:
+        spins.remove(0)  # zero not required here
+
+    return np.array(spins)
+
+def createLbam(sup, clusexp, mobile_occ, spins):
     """
 
     :param sup: supercell object, which is also the current state of the object
@@ -17,10 +28,4 @@ def createLbam(sup, clusexp, mobile_occ, spec_count=0):
     if not np.allclose(sum([arr for arr in mobile_occ]), 1):
         raise ValueError("The occupancies of sites do not add up to one")
 
-    # Now, we need to assign spins to each species - how to do this quickly
-    # get the halfpoint
-    m = (len(mobile_occ) + spec_count)//2
-    spins = list(range(-m, m+1))
-    if m % 2 == 1:
-        spins.remove(0)  # zero not required here
-    spins_mobile = spins[:(len(mobile_occ))]  # the last two spin variables belong to the occupied sites
+
