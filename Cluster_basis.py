7890f9477377c89fe953af5c1f8a441772e7178f
sohamch <ch.soham@gmail.com>
2020-01-31 15:09:49 -0600

    spectator spins not required. Can be absorbed in CI.

--- Cluster_basis.py
+++ Cluster_basis.py
@@ -16,35 +16,6 @@ def createSpins(mobile_count, spec_count=0):
 
     return np.array(spins)
 
-
-def specProducts(sup, clusexp, speclist, spins):
-    """
-    for a given occupancy, store the products of the spectator spins in each cluster.
-    This can be done only once and used over and over again, since spectators don't move.
-    If the length of speclist is n_spec, then the first n_spec spins in spins are assigned to the spectator species
-    """
-    if not np.allclose(sum([arr for arr in speclist]), 1):
-        raise ValueError("The occupancies of sites do not add up to one")
-
-    allClist = [cl for clist in clusexp for cl in clist]
-    Nclusters = len(clist)
-    PhiSpecArray = np.zeros((len(sup.Rvectlist), Nclusters))
-
-    # Go through the position vectors
-    for Rind, R in enumerate(sup.Rvectlist):
-        # Go through the clusters in the representatives
-        for clInd, cl in enumerate(allClist):
-            # add the translation to all the sites in the cluster and get the indices of those sites
-            # Verify this
-            sitelist = [sup.index(R, site)[0] for site in cl.sites]
-            # Next, get the spin products of the sites in sitelist
-            Phi_spec = np.prod(np.array([sum([spin*spec[ind] for spin, spec in zip(spins, speclist)])
-                                         for ind in sitelist]))
-            # Now store the spin products in the array
-
-            PhiSpecArray[Rind, clInd] = Phi_spec
-    return PhiSpecArray
-
 def DiffOcc(occlist1, occlist2):
     """
     Takes two occupancy lists and returns one that only contains 1 for sites that have exchanged.
@@ -53,14 +24,13 @@ def DiffOcc(occlist1, occlist2):
     diff = [np.abs(occ1 - occ2).astype(int) for occ1, occ2 in zip(occlist1, occlist2)]
     return diff
 
-def ClusterProducts(sup, clusexp, mobilelist, PhiSpecArray, spins):
+def ClusterProducts(sup, clusexp, mobilelist, spins):
     """
 
     :param sup: supercell object, which is also the current state of the object
     :param clusexp: list of clusters around supercell
-    :param mobile_occ: list of occupancies on mobile sites for each species that can occupy those sites
-    :param spec_count : how many spectator species do we have. This doesn't matter except in deciding spin values.
-    the lists should be so that sum_(site over all all sites) occupancy_(species)(site) = 1
+    :param mobilelist: list of occupancies on mobile sites for each species that can occupy those sites
+    "param spins : the spins of the chemical species - assume the spins of mobile species are stored first.
     :return: one-d array : corresponding to the values of the cluster basis functions for the different clusters.
     This needs to be used more than once, since the bias needs to be updated at every state.
     """
@@ -85,6 +55,8 @@ def ClusterProducts(sup, clusexp, mobilelist, PhiSpecArray, spins):
 
             PhiMobileArray[Rind, clInd] = Phi_mob
 
-    return PhiSpecArray * PhiMobileArray
+    return PhiMobileArray
 
-
+# Todo - Can we make the above computations more efficient. Imganine a case with a large nuumber of sites.
+# As we go from one state to another, only a few clusters change. So the Phi array for the next state
+# Can be evaluated from the Phi array of the current state easily. Check how.
