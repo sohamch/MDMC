ace177d22295278cae7be4e788e2d3ff1aee1148
sohamch <ch.soham@gmail.com>
2020-02-07 15:42:15 -0600

    implemented a global counting function. Now have to specialize it to changes in counts.

--- Cluster_basis.py
+++ Cluster_basis.py
@@ -55,7 +55,7 @@ def createClusterBasis(sup, clusexp, specList, mobList):
                 mobcount = collections.Counter(tup1)
                 if any(j > lenMob[i] for i, j in mobcount.items()):
                     continue
-                clusterGates.append((tup1, clistInd))
+                clusterGates.append((tup1, [], clistInd))
             return clusterGates
 
         for tup1 in arrangemobs:
@@ -71,3 +71,39 @@ def createClusterBasis(sup, clusexp, specList, mobList):
                     continue
                 clusterGates.append((tup1, tup2, clistInd))
     return clusterGates
+
+
+def countGates(sup, mobOcc, clusexp, clusterGates, specOcc=None):
+    """
+    Takes in a set of occupancy vectors, and returns counts of open cluster gates.
+    """
+    # TODO - Need to verify that this worls when specOcc = None
+    gateCounts = np.zeros(len(clusterGates), dtype=int)
+    for gateIndex, gate in enumerate(clusterGates):
+        MobSpecies = gate[0]
+        SpecSpecies = gate[1]
+        if specOcc is None and len(SpecSpecies) != 0:
+            raise TypeError("spectator occupancy passed as None, but spectator states specified in gates.")
+        clusterList = clusexp[gate[2]]
+
+        # Now translate the clusters
+        for Rtrans in sup.Rvectlist:
+            for clust in clusterList:
+                # The index function takes care of the modulus (periodicity).
+                Mobsitesnew = [sup.index(site.R+Rtrans, site.ci)[0] for site in cluster.sites
+                               if site.ci in sup.indexmobile]
+                Specsitesnew = []
+                if specOcc is not None:
+                    Specsitesnew = [sup.index(site.R + Rtrans, site.ci)[0] for site in cluster.sites
+                                    if site.ci in sup.indexspectator]
+                # Now get the occupancies at each site
+                species_mob = tuple([sum([i*occlist[j] for i, occlist in enumerate(mobOcc)]) for j in Mobsitesnew])
+                species_spec = tuple([sum([i * occlist[j] for i, occlist in enumerate(specOcc)]) for j in Specsitesnew])
+                if specOcc is None:
+                    if species_mob == MobSpecies:
+                        gateCounts[gateIndex] += 1
+                else:
+                    if species_mob == MobSpecies and species_spec == SpecSpecies:
+                        gateCounts[gateIndex] += 1
+
+
