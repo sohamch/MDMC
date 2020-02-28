9b34d3591d498ec713f9af09aa6cad83d415f39f
sohamch <ch.soham@gmail.com>
2020-02-28 17:05:40 -0600

    defining KRA calculation

--- Transitions.py
+++ Transitions.py
@@ -1,6 +1,7 @@
 from onsager import crystal, cluster, supercell
 import numpy as np
-
+import itertools
+import collections
 """
 This is to create KRA expansions of activation barriers. We assume for now that the expansion coefficients are given.
 """
@@ -23,11 +24,12 @@ class KRAExpand(object):
     """
     Object that contains all information regarding the KRA expansion of a jumpnetwork in a supercell.
     """
-    def __init__(self, sup, chem, jumpnetwork, clusexp, InteractionCutoff):
+    def __init__(self, sup, chem, jumpnetwork, clusexp, sampleState, InteractionCutoff):
         """
         :param sup: clusterSupercell Object
         :param chem: the sublattice index on which the jumpnetwork has been built.
         :param jumpnetwork: jumpnetwork to expand
+        :param sampleState : A sample state to count the number of atomic species of each jump
         :param clusexp: representative set of clusters - out put of make clusters function.
         :param InteractionCutoff: the maximum distance of a cluster from a transition to consider
         """
@@ -40,6 +42,7 @@ class KRAExpand(object):
 
         # First, we reform the jumpnetwork
         self.clusterJumps = self.reDefineJumps()
+        self.clusterSpeciesJumps = self.defineSpecies(sampleState)
 
     def reDefineJumps(self):
         """
@@ -89,6 +92,7 @@ class KRAExpand(object):
                         # The new cluster should not contain the initial or final site of the jump.
                         if siteA in newsiteList or siteB in newsiteList:
                             continue
+                        # TODO - test that none of the final clusters contain the initial or final site.
                         # Get distances of new sites from initial and final state
                         dists_i = [np.dot(self.crys.lattice, (site.R - siteA.R + self.crys.basis[site.ci[0]][site.ci[1]]
                                                               - self.crys.basis[siteA.ci[0]][siteA.ci[1]]))
@@ -104,16 +108,15 @@ class KRAExpand(object):
                             newsymList = [newClust.g(self.crys, gop) for gop in ijPtGroup]
                             ijClexp.append(newsymList)
                             clTracker.update(newsymList)
-
                 clusterjumplist[(siteA, siteB)] = ijClexp
                 
         return clusterjumplist
 
-    def defineSpecies(self, Nmobile):
+    def defineSpecies(self, mobOccs):
         """
         Used to assign chemical species to the jump cluster expansions.
-        :param Nmobile: number of mobile species in the lattice, including the carrier. It is assumed that the species
-        are labelled with numbers and that the number Nmobile-1 is the label for the carrier.
+
+        :param: mobOccs - this is the starting/initial state and is only used to get the number of species of each kind.
 
         We'll have separate expansions for every species that occupies the final site of a carrier jump,
         and every cluster for a given jump will have species assigned to it.
@@ -121,17 +124,54 @@ class KRAExpand(object):
         :returns  SpecClusterJumpList - a cluster expansion for KRA with species assigned to the clusters.
         """
 
+        Nmobile = len(mobOccs)
+        NumSpecs = [np.sum(OccArr) for OccArr in mobOccs]
         clusterJumps = getattr(self, "clusterJumps", None)
         if clusterJumps is None:
             raise ValueError("Need to have generated cluster expansions for the jumps first.")
 
+        mobileSpecs = tuple(range(Nmobile-1))  # the last species is the vacancy, so we are not considering it.
         clusterJumpsSpecies = {}
         for AB, clusterSymLists in clusterJumps.items():
             # For this transition, first assign species to the clusters
+            AtomicClusterSymList = []
+            for clusterList in clusterSymLists:
+                cl0 = clusterList[0].cluster
+                # Get the order of the cluster and assign species to the sites
+                Specs = itertools.product(mobileSpecs, repeat=cl0.Norder)
+                for tup in Specs:
+                    newsymlist = []
+                    # Check if the number of atoms crosses the total number of atoms of a species.
+                    MobNumber = collections.Counter(tup)
+                    if any(NumSpecs[i] < j for i, j in MobNumber.items()):
+                        continue
+                    AtomicClusterSymList.append([tup, clusterList])
             # use itertools.product like in normal cluster expansion.
-
             # Then, assign species to te final site of the jumps.
             for specJ in range(Nmobile-1):
                 ABspecJ = (AB[0], AB[1], specJ)
+                clusterJumpsSpecies[ABspecJ] = AtomicClusterSymList
 
+        return clusterJumpsSpecies
 
+    def getdelE_KRA(self, transition, mobOcc, specJ, KRACoeffs):
+        """
+        Given a transition and a state, get the KRA activation value for that jump in that state.
+        During testing, we'll assume that fitting KRA coefficients has already been done.
+        :param transition: the transition in the form of ((i,j), dx)
+        :param mobOcc: the state as an array of occupancy vectors.
+        :param specJ: the species that exchanges with the vacancies.
+        :param KRACoeffs: the KRA coefficients for that type of jump, arranged appropriately for each cluster.
+        :return: The Calculated KRA activation energy.
+        """
+        i, j, dx = transition[0][0], transition[0][1], transition[1]
+        siteA = self.sup.index((self.chem, i), np.zeros(3, dtype=int))
+        Rj, (c, cj) = self.crys.cart2pos(dx + np.dot(self.crys.lattice, self.crys.basis[self.chem][j]))
+        # check we have the correct site
+        if not cj == j:
+            raise ValueError("improper coordinate transformation, did not get same site")
+        siteB = self.sup.index((self.chem, j), Rj)
+
+        key = (siteA, siteB, specJ)
+
+        # SymClusterlists =
