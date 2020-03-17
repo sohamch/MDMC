fixed calculation of Rj. Need to test.

Cluster_Expansion.py
@@ -232,7 +232,9 @@ class VectorClusterExpansion(object):
         dxList = []
         for jump in [jmp for jList in jumpnetwork for jmp in jList]:
             siteA = self.sup.index((self.chem, jump[0][0]), np.zeros(3, dtype=int))
-            Rj, (c, cj) = self.crys.cart2pos(jump[1] + np.dot(self.crys.lattice, self.crys.basis[self.chem][jump[0][1]]))
+            Rj, (c, cj) = self.crys.cart2pos(jump[1] -
+                                             np.dot(self.crys.lattice, self.crys.basis[self.chem][jump[0][1]]) -
+                                             np.dot(self.crys.lattice, self.crys.basis[self.chem][jump[0][0]]))
             # check we have the correct site
             if not cj == jump[0][1]:
                 raise ValueError("improper coordinate transformation, did not get same site")