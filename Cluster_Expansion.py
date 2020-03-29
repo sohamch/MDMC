7fb88125f1ed2505e1ce52a6ec4807f2a1b93004
sohamch <ch.soham@gmail.com>
2020-03-28 20:51:29 -0500

    basis uniqueness working fine for 2 body

--- Cluster_Expansion.py
+++ Cluster_Expansion.py
@@ -3,6 +3,8 @@ import numpy as np
 import collections
 import itertools
 import Transitions
+import time
+
 
 class ClusterSpecies():
 
@@ -16,8 +18,9 @@ class ClusterSpecies():
         self.specList = specList
         self.siteList = siteList
         Rtrans = sum([site.R for site in siteList])//len(siteList)
-        self.SiteSpecs = set([(site-Rtrans, spec) for site, spec in zip(siteList, specList)])
-        self.__hashcache__ = sum([hash((site-Rtrans, spec))for site, spec in zip(siteList, specList)])
+        self.transPairs = [(site-Rtrans, spec) for site, spec in zip(siteList, specList)]
+        self.SiteSpecs = set(self.transPairs)
+        self.__hashcache__ = int(np.prod(np.array([hash((site, spec)) for site, spec in self.transPairs])))
 
     def __eq__(self, other):
         if self.SiteSpecs == other.SiteSpecs:
@@ -32,8 +35,8 @@ class ClusterSpecies():
 
     def strRep(self):
         str= ""
-        for site, spec in zip(self.siteList, self.specList):
-            str += "Spec:{}, site:{},{}".format(spec, site.ci, site.R)
+        for site, spec in self.SiteSpecs:
+            str += "Spec:{}, site:{},{} ".format(spec, site.ci, site.R)
         return str
 
     def __repr__(self):
@@ -65,60 +68,74 @@ class VectorClusterExpansion(object):
         self.mobList = list(range(len(mobCountList)))
         self.vacSite = vacSite
         # TODO - think of a better way to do this.
-        self.genVecs()
+        start = time.time()
+        self.SpecClusters = self.recalcClusters()
+        print("Created clusters with atomic species: {}".format(time.time() - start))
+        # self.genVecs()
+        start = time.time()
         self.ScalarBasis = self.createScalarBasis()
-        # Generate the complete cluster basis including the
-        # arrangement of species on sites other than the vacancy site.
-        self.index()
+        print("Created clusters with atomic species: {}".format(time.time() - start))
+        # Generate the complete cluster basis including the arrangement of species on sites other than the vacancy site.
         self.KRAexpander = Transitions.KRAExpand(sup, self.chem, jumpnetwork, clusexp, mobCountList)
 
     def recalcClusters(self):
         """
         Intended to take in a site based cluster expansion and recalculate the clusters with species in them
         """
-        considered = set()
-        for clList in self.clusexp:
-            for clust in clList:
+        allClusts = set()
+        symClusterList = []
+        for clSet in self.clusexp:
+            for clust in list(clSet):
                 Nsites = len(clust.sites)
-                occupancies = itertools.product(self.mobList, repeat=Nsites)
+                occupancies = list(itertools.product(self.mobList, repeat=Nsites))
                 for siteOcc in occupancies:
                     # Make the cluster site object
                     ClustSpec = ClusterSpecies(siteOcc, clust.sites)
                     # check if this has already been considered
-                    if ClustSpec in considered:
+                    if ClustSpec in allClusts:
+                        continue
+                    # Check if number of each species in the cluster is okay
+                    mobcount = collections.Counter(siteOcc)
+                    # Check if the number of atoms of a given species does not exceed the total number of atoms of that
+                    # species in the solid.
+                    if any(j > self.mobCountList[i] for i, j in mobcount.items()):
                         continue
                     # Otherwise, find all symmetry-grouped counterparts
+                    newSymSet = set([ClustSpec.g(self.crys, g) for g in self.crys.G])
+                    allClusts.update(newSymSet)
+                    newList = list(newSymSet)
+                    symClusterList.append(newList)
 
+        return symClusterList
 
-
-    def genVecs(self):
-        """
-        Function to generate a symmetry-grouped vector cluster expansion similar to vector stars in the onsager code.
-        """
-        sup = self.sup
-        clusexp = self.clusexp
-        Id3 = np.eye(3)
-        self.SymVecClustList = []
-        self.VclusterSupIndList = []
-        self.SymVecList = []
-        for clist in clusexp:
-            cl0 = list(clist)[0]
-            for vec in Id3:
-                symclList = [cl0]
-                symcLSupIndList = []
-                symvecList = [vec]
-                for cl in clist:
-                    for gop in sup.crys.G:
-                        if cl0.g(sup.crys, gop) == cl:
-                            if any(cl1 == cl for cl1 in symclList):
-                                continue
-                            symclList.append(cl)
-                            symvecList.append(np.dot(gop.cartrot, vec))
-                            symcLSupIndList.append([self.sup.index(site.R, site.ci) for site in cl.sites])
-
-                self.SymVecClustList.append(symclList)
-                self.SymVecList.append(symvecList)
-                self.VclusterSupIndList.append(symcLSupIndList)
+    # def genVecs(self):
+    #     """
+    #     Function to generate a symmetry-grouped vector cluster expansion similar to vector stars in the onsager code.
+    #     """
+    #     sup = self.sup
+    #     clusexp = self.clusexp
+    #     Id3 = np.eye(3)
+    #     self.SymVecClustList = []
+    #     self.VclusterSupIndList = []
+    #     self.SymVecList = []
+    #     for clist in clusexp:
+    #         cl0 = list(clist)[0]
+    #         for vec in Id3:
+    #             symclList = [cl0]
+    #             symcLSupIndList = []
+    #             symvecList = [vec]
+    #             for cl in clist:
+    #                 for gop in sup.crys.G:
+    #                     if cl0.g(sup.crys, gop) == cl:
+    #                         if any(cl1 == cl for cl1 in symclList):
+    #                             continue
+    #                         symclList.append(cl)
+    #                         symvecList.append(np.dot(gop.cartrot, vec))
+    #                         symcLSupIndList.append([self.sup.index(site.R, site.ci) for site in cl.sites])
+    #
+    #             self.SymVecClustList.append(symclList)
+    #             self.SymVecList.append(symvecList)
+    #             self.VclusterSupIndList.append(symcLSupIndList)
 
     # def index(self):
     #     """
@@ -179,126 +196,127 @@ class VectorClusterExpansion(object):
                     continue
                 # Each cluster is associated with three vectors
                 clusterBasis.append((tup, clistInd))
+
         return clusterBasis
 
-    def Expand(self, beta, mobOccs, transitions, EnCoeffs, KRACoeffs):
-
-        """
-        :param beta : 1/KB*T
-        :param mobOccs: the mobile occupancy in the current state
-        :param transitions: the jumps out of the current state - supercell indices for initial and final sites
-        :param EnCoeffs: energy interaction coefficients in a cluster expansion
-        :param KRACoeffs: kinetic energy coefficients - pre-formed
-        :return: Wbar, Bbar - rate and bias expansions in the cluster basis
-        """
-
-        ijlist, dxlist = transitions
-        mobOccs_final = mobOccs.copy()
-
-        del_lamb_mat = np.zeros((len(self.FullClusterBasis), len(self.FullClusterBasis), len(ijlist)))
-        delxDotdelLamb = np.zeros((len(self.FullClusterBasis), len(ijlist)))
-
-        # To be tensor dotted with ratelist with axes = (0,1)
-        ratelist = np.zeros(len(ijlist))
-
-        for (jnum, ij, dx) in zip(itertools.count(), ijlist, dxlist):
-
-            del_lamb = np.zeros((len(self.FullClusterBasis), 3))
-
-            specJ = sum([occ[ij[1]]*label for occ, label in zip(mobOccs, self.mobList)])
-            # siteJ = self.sup.ciR(ij[1])  # get the lattice site where the jumping species initially sits
-
-            # Get the KRA energy for this jump
-            delEKRA = self.KRAexpander.GetKRA((ij, dx), mobOccs, KRACoeffs[(ij[0], ij[1], specJ)])
-            delE = 0.0  # This will added to the KRA energy to get the activation barrier
-
-            # switch the occupancies in the final state
-            mobOccs_final[specJ][ij[0]] = 1
-            mobOccs_final[specJ][ij[1]] = 0
-
-            mobOccs_final[-1][ij[0]] = 0
-            mobOccs_final[specJ][ij[1]] = 1
-
-            # delOcc = mobOccs_final - mobOccs  # Doesn't seem to be much we can do with this
-
-            # Get all the clusters that contain the vacancy at the vacancy site and/or specJ at ij[1]
-            # and    are On in the initial state.
-
-            InitOnClustersVac = [(bInd, clInd) for bInd, clInd, siteInd in self.SupInd2VClus[ij[0]]
-                                 if all([mobOccs[species][idx] == 1
-                                            for species, idx in zip(self.FullClusterBasis[bInd][0],
-                                                                    self.VclusterSupIndList[bInd][clInd])])
-                                 ]
-
-            InitOnClustersSpecJ = [(bInd, clInd) for bInd, clInd, siteInd in self.SupInd2VClus[ij[1]]
-                                   if all([mobOccs[species][idx] == 1
-                                              for species, idx in zip(self.FullClusterBasis[bInd][0],
-                                                                      self.VclusterSupIndList[bInd][clInd])])
-                                   ]
-
-            FinOnClustersVac = [(bInd, clInd) for bInd, clInd, siteInd in self.SupInd2VClus[ij[0]]
-                                if all([mobOccs_final[species][idx] == 1
-                                           for species, idx in zip(self.FullClusterBasis[bInd][0],
-                                                                   self.VclusterSupIndList[bInd][clInd])])
-                                ]
-
-            FinOnClustersSpecJ = [(bInd, clInd) for bInd, clInd, siteInd in self.SupInd2VClus[ij[1]]
-                                  if all([mobOccs_final[species][idx] == 1
-                                             for species, idx in zip(self.FullClusterBasis[bInd][0],
-                                                                     self.VclusterSupIndList[bInd][clInd])])
-                                  ]
-
-            # Turn of the On clusters
-            for (bInd, clInd) in set(InitOnClustersVac).union(set(InitOnClustersSpecJ)):
-                del_lamb[bInd] -= self.SymVecList[bInd][clInd]
-                delE -= EnCoeffs[self.ScalarBasis[bInd//3]]
-
-            # Turn on the Off clusters
-            for (bInd, clInd) in set(FinOnClustersVac).union(FinOnClustersSpecJ):
-                del_lamb[bInd] += self.SymVecList[bInd][clInd]
-                delE += EnCoeffs[self.ScalarBasis[bInd//3]]
-
-            # append to the rateList
-            ratelist[jnum] = np.exp(-(0.5*delE + delEKRA))
-
-            # Create the matrix to find Wbar
-            del_lamb_mat[:, :, jnum] = np.dot(del_lamb, del_lamb.T)
-
-            # Create the matrix to find Bbar
-            delxDotdelLamb[:, jnum] = np.tensordot(del_lamb_mat, dx, axes=(1, 0))
-
-        Wbar = np.tensordot(ratelist, del_lamb_mat, axes=(0, 0))
-        Bbar = np.tensordot(ratelist, delxDotdelLamb, axes=(0, 1))
-
-        return Wbar, Bbar
-
-    def transitions(self, mobOcc, jumpnetwork):
-        """
-        Function to calculate the transitions and their rates out of a given state.
-        :param mobOcc: occupancy vectors for mobile species in the current state
-        :param jumpnetwork: vacancy jumpnetwork
-        :return: (ijlist, dxlist)
-        """
-        # TODO - ijlist, dxlist will be calculated from the lattice, ratelist will be calculated from Transitions
-
-        ijList = []
-        dxList = []
-        for jump in [jmp for jList in jumpnetwork for jmp in jList]:
-            siteA = self.sup.index((self.chem, jump[0][0]), np.zeros(3, dtype=int))
-            Rj, (c, cj) = self.crys.cart2pos(jump[1] -
-                                             np.dot(self.crys.lattice, self.crys.basis[self.chem][jump[0][1]]) -
-                                             np.dot(self.crys.lattice, self.crys.basis[self.chem][jump[0][0]]))
-            # check we have the correct site
-            if not cj == jump[0][1]:
-                raise ValueError("improper coordinate transformation, did not get same site")
-            siteB = self.sup.index((self.chem, jump[0][1]), Rj)
-
-            specJ = np.prod(np.array([mobOcc[spec][siteB] for spec in self.mobList]))
-
-            ijList.append((siteA, siteB, jump[1]))
-            dxList.append(jump[1])
-
-        return ijList, dxList
+    # def Expand(self, beta, mobOccs, transitions, EnCoeffs, KRACoeffs):
+    #
+    #     """
+    #     :param beta : 1/KB*T
+    #     :param mobOccs: the mobile occupancy in the current state
+    #     :param transitions: the jumps out of the current state - supercell indices for initial and final sites
+    #     :param EnCoeffs: energy interaction coefficients in a cluster expansion
+    #     :param KRACoeffs: kinetic energy coefficients - pre-formed
+    #     :return: Wbar, Bbar - rate and bias expansions in the cluster basis
+    #     """
+    #
+    #     ijlist, dxlist = transitions
+    #     mobOccs_final = mobOccs.copy()
+    #
+    #     del_lamb_mat = np.zeros((len(self.FullClusterBasis), len(self.FullClusterBasis), len(ijlist)))
+    #     delxDotdelLamb = np.zeros((len(self.FullClusterBasis), len(ijlist)))
+    #
+    #     # To be tensor dotted with ratelist with axes = (0,1)
+    #     ratelist = np.zeros(len(ijlist))
+    #
+    #     for (jnum, ij, dx) in zip(itertools.count(), ijlist, dxlist):
+    #
+    #         del_lamb = np.zeros((len(self.FullClusterBasis), 3))
+    #
+    #         specJ = sum([occ[ij[1]]*label for occ, label in zip(mobOccs, self.mobList)])
+    #         # siteJ = self.sup.ciR(ij[1])  # get the lattice site where the jumping species initially sits
+    #
+    #         # Get the KRA energy for this jump
+    #         delEKRA = self.KRAexpander.GetKRA((ij, dx), mobOccs, KRACoeffs[(ij[0], ij[1], specJ)])
+    #         delE = 0.0  # This will added to the KRA energy to get the activation barrier
+    #
+    #         # switch the occupancies in the final state
+    #         mobOccs_final[specJ][ij[0]] = 1
+    #         mobOccs_final[specJ][ij[1]] = 0
+    #
+    #         mobOccs_final[-1][ij[0]] = 0
+    #         mobOccs_final[specJ][ij[1]] = 1
+    #
+    #         # delOcc = mobOccs_final - mobOccs  # Doesn't seem to be much we can do with this
+    #
+    #         # Get all the clusters that contain the vacancy at the vacancy site and/or specJ at ij[1]
+    #         # and    are On in the initial state.
+    #
+    #         InitOnClustersVac = [(bInd, clInd) for bInd, clInd, siteInd in self.SupInd2VClus[ij[0]]
+    #                              if all([mobOccs[species][idx] == 1
+    #                                         for species, idx in zip(self.FullClusterBasis[bInd][0],
+    #                                                                 self.VclusterSupIndList[bInd][clInd])])
+    #                              ]
+    #
+    #         InitOnClustersSpecJ = [(bInd, clInd) for bInd, clInd, siteInd in self.SupInd2VClus[ij[1]]
+    #                                if all([mobOccs[species][idx] == 1
+    #                                           for species, idx in zip(self.FullClusterBasis[bInd][0],
+    #                                                                   self.VclusterSupIndList[bInd][clInd])])
+    #                                ]
+    #
+    #         FinOnClustersVac = [(bInd, clInd) for bInd, clInd, siteInd in self.SupInd2VClus[ij[0]]
+    #                             if all([mobOccs_final[species][idx] == 1
+    #                                        for species, idx in zip(self.FullClusterBasis[bInd][0],
+    #                                                                self.VclusterSupIndList[bInd][clInd])])
+    #                             ]
+    #
+    #         FinOnClustersSpecJ = [(bInd, clInd) for bInd, clInd, siteInd in self.SupInd2VClus[ij[1]]
+    #                               if all([mobOccs_final[species][idx] == 1
+    #                                          for species, idx in zip(self.FullClusterBasis[bInd][0],
+    #                                                                  self.VclusterSupIndList[bInd][clInd])])
+    #                               ]
+    #
+    #         # Turn of the On clusters
+    #         for (bInd, clInd) in set(InitOnClustersVac).union(set(InitOnClustersSpecJ)):
+    #             del_lamb[bInd] -= self.SymVecList[bInd][clInd]
+    #             delE -= EnCoeffs[self.ScalarBasis[bInd//3]]
+    #
+    #         # Turn on the Off clusters
+    #         for (bInd, clInd) in set(FinOnClustersVac).union(FinOnClustersSpecJ):
+    #             del_lamb[bInd] += self.SymVecList[bInd][clInd]
+    #             delE += EnCoeffs[self.ScalarBasis[bInd//3]]
+    #
+    #         # append to the rateList
+    #         ratelist[jnum] = np.exp(-(0.5*delE + delEKRA))
+    #
+    #         # Create the matrix to find Wbar
+    #         del_lamb_mat[:, :, jnum] = np.dot(del_lamb, del_lamb.T)
+    #
+    #         # Create the matrix to find Bbar
+    #         delxDotdelLamb[:, jnum] = np.tensordot(del_lamb_mat, dx, axes=(1, 0))
+    #
+    #     Wbar = np.tensordot(ratelist, del_lamb_mat, axes=(0, 0))
+    #     Bbar = np.tensordot(ratelist, delxDotdelLamb, axes=(0, 1))
+    #
+    #     return Wbar, Bbar
+    #
+    # def transitions(self, mobOcc, jumpnetwork):
+    #     """
+    #     Function to calculate the transitions and their rates out of a given state.
+    #     :param mobOcc: occupancy vectors for mobile species in the current state
+    #     :param jumpnetwork: vacancy jumpnetwork
+    #     :return: (ijlist, dxlist)
+    #     """
+    #     # TODO - ijlist, dxlist will be calculated from the lattice, ratelist will be calculated from Transitions
+    #
+    #     ijList = []
+    #     dxList = []
+    #     for jump in [jmp for jList in jumpnetwork for jmp in jList]:
+    #         siteA = self.sup.index((self.chem, jump[0][0]), np.zeros(3, dtype=int))
+    #         Rj, (c, cj) = self.crys.cart2pos(jump[1] -
+    #                                          np.dot(self.crys.lattice, self.crys.basis[self.chem][jump[0][1]]) +
+    #                                          np.dot(self.crys.lattice, self.crys.basis[self.chem][jump[0][0]]))
+    #         # check we have the correct site
+    #         if not cj == jump[0][1]:
+    #             raise ValueError("improper coordinate transformation, did not get same site")
+    #         siteB = self.sup.index((self.chem, jump[0][1]), Rj)
+    #
+    #         specJ = np.prod(np.array([mobOcc[spec][siteB] for spec in self.mobList]))
+    #
+    #         ijList.append((siteA, siteB, jump[1]))
+    #         dxList.append(jump[1])
+    #
+    #     return ijList, dxList
 
 
 
