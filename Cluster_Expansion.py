501752548641dedcc9d8de742e6730fc85d32cf7
sohamch <ch.soham@gmail.com>
2020-07-16 22:10:03 -0500

    using new method to generate interactions.

--- Cluster_Expansion.py
+++ Cluster_Expansion.py
@@ -209,8 +209,6 @@ class VectorClusterExpansion(object):
         generate interactions for every site - for MC moves
         """
         SiteSpecinteractList = collections.defaultdict(list)
-        count = 0
-        maxinteractions = 0
         for siteInd in range(self.Nsites):
             # get the cluster site
             ci, R = self.sup.ciR(siteInd)
@@ -218,16 +216,14 @@ class VectorClusterExpansion(object):
             # Now, go through all the clusters
             for cl in [cl for clist in self.SpecClusters for cl in clist]:
                 for site, spec in cl.SiteSpecs:
-                    count += 1
-                    # if site.ci == ci: In our case we have only one chemistry.
-                    Rtrans = R - site.R
-                    interactSupInd = tuple([(self.sup.index(site.R+Rtrans, site.ci)[0], spec)
-                                            for site, spec in cl.SiteSpecs])
-                    # # this check is to account for periodic boundary conditions
-                    SiteSpecinteractList[(clSite, spec)].append([interactSupInd, cl, Rtrans])
+                    if site.ci == ci: # In our case we have only one chemistry.
+                        Rtrans = R - site.R
+                        interactSupInd = tuple([(self.sup.index(site.R+Rtrans, site.ci)[0], spec)
+                                                for site, spec in cl.SiteSpecs])
+                        # # this check is to account for periodic boundary conditions
+                        SiteSpecinteractList[(clSite, spec)].append([interactSupInd, cl, Rtrans])
 
         maxinteractions = max([len(lst) for key, lst in SiteSpecinteractList.items()])
-        print("total loop iterations : {}".format(count))
         return SiteSpecinteractList, maxinteractions
 
     def makeJitInteractionsData(self, Energies):
