35505be52ac613440cc57f26c8cd46b4b6f1795e
sohamch <ch.soham@gmail.com>
2020-07-16 22:26:01 -0500

    trying to get rid of redundant vacancy containing clusters.

--- Cluster_Expansion.py
+++ Cluster_Expansion.py
@@ -98,7 +98,11 @@ class VectorClusterExpansion(object):
 
     def recalcClusters(self):
         """
-        Intended to take in a site based cluster expansion and recalculate the clusters with species in them
+        Intended to take in a site based cluster expansion and recalculate the clusters
+        by populating the site with species.
+
+        In all our simulations, we'll keep the vacancy fixed at a particular site, so we will not consider
+        any cluster where the vacancy has been assigned to any other site.
         """
         allClusts = set()
         symClusterList = []
