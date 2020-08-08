8fa3d9db1e93cbeac8fb0919d1294d3348d023a0
sohamch <ch.soham@gmail.com>
2020-08-07 23:14:32 -0500

    Need to update vector basis tests, but they aren't needed now.

--- test_MC.py
+++ test_MC.py
@@ -89,18 +89,21 @@ class Test_MC_Arrays(unittest.TestCase):
             # Now, get the representative cluster
             repClus = InteractionRepClusDict[interaction]
 
+            clInd = self.VclusExp.cl2clInd[repClus]
+            self.assertEqual(InteractToRepClus[i], clInd)
+
             # test the energy index
             enIndex = self.VclusExp.clust2SpecClus[repClus][0]
             self.assertEqual(Interaction2En[i], self.Energies[enIndex])
 
-            # get the vector basis info for this cluster
-            vecList = self.VclusExp.clust2vecClus[repClus]
-            # check the number of vectors
-            self.assertEqual(numVecsInteracts[i], len(vecList))
-            # check that the correct vector have been stored, in the same order as in vecList (not necessary but short testing)
-            for vecind in range(len(vecList)):
-                vec = self.VclusExp.vecVec[vecList[vecind][0]][vecList[vecind][1]]
-                self.assertTrue(np.allclose(vec, VecsInteracts[i, vecind, :]))
+            # # get the vector basis info for this cluster
+            # vecList = self.VclusExp.clust2vecClus[repClus]
+            # # check the number of vectors
+            # self.assertEqual(numVecsInteracts[i], len(vecList))
+            # # check that the correct vector have been stored, in the same order as in vecList (not necessary but short testing)
+            # for vecind in range(len(vecList)):
+            #     vec = self.VclusExp.vecVec[vecList[vecind][0]][vecList[vecind][1]]
+            #     self.assertTrue(np.allclose(vec, VecsInteracts[i, vecind, :]))
 
         # Next, test the interactions each (site, spec) is a part of
         self.assertEqual(numInteractsSiteSpec.shape[0], len(self.superBCC.mobilepos))
