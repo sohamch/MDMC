caf44f4376daa1e81a62974825aec419eb0d80a7
sohamch <ch.soham@gmail.com>
2021-08-21 16:20:35 -0500

    Initializing the new class.

--- KRA_monoatomic.py
+++ KRA_monoatomic.py
@@ -8,10 +8,24 @@ class KRA_expansion():
     This is to compute KRA cluster expansion involving sites up to a cutoff and transition sites
     to be taken from a jumpnetwork in mono-atomic lattices.
     """
-    def __init__(self, sup, jnet, cutoff):
+    def __init__(self, sup, jnet, chem, cutoff):
         """
         :param sup: the supercell object based on which sites will be given indices
         :param jnet: the jumpnetwork from transition sites are taken
+        :param chem : the sublattice on which the vacancy jumps
         :param cutoff: the maximum distance up to which sites will be considered
         (from either the vacancy (at 0,0,0) or the transition sites
         """
+        self.sup = sup
+        self.jnet = jnet
+        self.cutoff = cutoff
+        self.crys = sup.crys
+        # first let's get the jump sites
+        jList = []
+        dxList = []
+        for jList in jnet:
+            newList = []
+            for (i, j), dx in jList:
+                dxR, (chem, cj) = sup.crys.cart2pos(dx)
+                assert j == cj
+                siteInd = sup.index()
