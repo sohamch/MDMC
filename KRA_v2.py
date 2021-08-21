de386968c212096f2a95c722646175f491aa5ba6
sohamch <ch.soham@gmail.com>
2021-08-21 17:04:35 -0500

    adding jumpnetwork, test notebook and order.

--- KRA_v2.py
+++ KRA_v2.py
@@ -3,29 +3,60 @@ import numpy as np
 import itertools
 import collections
 
+# Test notebooks for this file:
+# "Test_site_generation"
+
 class KRA_expansion():
     """
     This is to compute KRA cluster expansion involving sites up to a cutoff and transition sites
     to be taken from a jumpnetwork in mono-atomic lattices.
     """
-    def __init__(self, sup, jnet, chem, cutoff):
+    def __init__(self, sup, jnet, chem, order, cutoff):
         """
         :param sup: the supercell object based on which sites will be given indices
         :param jnet: the jumpnetwork from transition sites are taken
         :param chem : the sublattice on which the vacancy jumps
+        :param order : the max number of sites in the clusters (excluding the transition sites)
         :param cutoff: the maximum distance up to which sites will be considered
         (from either the vacancy (at 0,0,0) or the transition sites
         """
         self.sup = sup
         self.jnet = jnet
+        self.chem = chem
+        self.order = order
         self.cutoff = cutoff
         self.crys = sup.crys
-        # first let's get the jump sites
+        self.IndexJumps()
+
+    def IndexJumps(self):
         jList = []
         dxList = []
-        for jList in jnet:
-            newList = []
-            for (i, j), dx in jList:
-                dxR, (chem, cj) = sup.crys.cart2pos(dx)
+        nJumpSets = len(self.jnet)
+
+        for jumplist in self.jnet:
+            newJList = []
+            newdxList = []
+            for (i, j), dx in jumplist:
+                dxR, (ch, cj) = self.sup.crys.cart2pos(dx)
                 assert j == cj
-                siteInd = sup.index()
+                assert ch == self.chem
+                siteInd, mob = self.sup.index(dxR, (ch, cj))
+                # the site must be mobile if a vacancy can jump to it
+                assert mob, "Check supercell. Mobile site not detected."
+
+                if nJumpSets == 1:  # if there is only a single set of symmetry-related jumps
+                    jList.append(siteInd)
+                    dxList.append(dx)
+                else:
+                    newJList.append(siteInd)
+                    newdxList.append(dx)
+
+            if nJumpSets > 1:
+                jList.append(newJList)
+                dxList.append(newdxList)
+
+        self.jList = jList
+        self.dxList = dxList
+
+    # Now let's generate all the sites required
+
