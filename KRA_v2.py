f0b2cd4bbd42cfc72dd7f2d342ba528e032984f8
sohamch <ch.soham@gmail.com>
2021-08-27 17:25:55 -0500

    Adding nearest neighbor lattice vectors.

--- KRA_v2.py
+++ KRA_v2.py
@@ -58,5 +58,61 @@ class KRA_expansion():
         self.jList = jList
         self.dxList = dxList
 
-    # Now let's generate all the sites required - we'll generate the sites independently
-    # and return an error if they periodically collide
+    # Now let's generate all the sites required - we'll generate the sites from a jump network
+    def GenerateInteractionSites(self, combinedShellRange, nnRange, cutoff):
+        """
+        Function to generation three-body transition state clusters grouped according to nearest neighborhoods
+        within the combined first neighbor unit cell from the initial and final transition states.
+        This is based on the paper 'doi.org/10.1016/j.msea.2018.11.064' and is applicable to ONLY monoatomic
+        lattices.
+        :param combinedShellRange: which combined shell of the initial and final atom should the third atom be within
+        :param nnRange: max nearest neighbor range to search within the sites that are in combinedShellRange
+        :param cutoff : the cutoff distance to get to nnrange
+        :return: Trnas2NNGroups: a dictionary containing transitions as keys and interaction sites as outputs.
+        """
+
+        # First we generate a jumpnetwork containing nn translation vectors upto nn range
+        nnJnet = self.crys.jumpnetwork(self.chem, cutoff)
+        nnVecs = [[np.dot(np.linalg.inv(self.crys.lattice), dx).astype(int) for (i, j), dx in jList]
+                              for jList in nnJnet]
+
+        Trans2NNGroups = {}
+        z = np.zeros(3, dtype=int)
+        chem = self.chem
+        for jList in self.jnet:
+            for (i, j), dx in jList:
+                siteA = cluster.ClusterSite(ci=(chem, i), R=z)
+                dxR, _ = self.crys.cart2pos(dx)
+                siteB = cluster.ClusterSite(ci=(chem, j), R=dxR)
+
+                # First, make the combined first nearest shell of both siteA and siteB
+                siteAshell1 = set()
+                siteBshell1 = set()
+                for nnT in nnVecs[:combinedShellRange]:
+                    siteAshell1.add(siteA + nnT)
+                    siteBshell1.add(siteB + nnT)
+                combShell = siteAshell1.union(siteBshell1)
+
+                # Now go through nn translations
+                nnGroups = {}
+                for nn in range(nnRange):
+                    siteAnn = set()
+                    siteBnn = set()
+                    for vec in nnVecs[nn]:
+                        siteAvec = siteA + vec
+                        siteBvec = siteB + vec
+                        # check that this is within 1nn of either A or B
+                        if siteAvec in combShell:
+                            siteAnn.add(siteAvec)
+                        if siteBvec in combShell:
+                            siteBnn.add(siteBvec)
+
+                    if nn == 0:  # for first nearest neighbor we need intersection
+                        nnGroups[nn + 1] = siteAnn.intersection(siteBnn)
+                    else: # for the second nearest neighbor onwards, union
+                        nnGroups[nn + 1] = siteAnn.union(siteBnn)
+
+                Trans2NNGroups[(siteA, siteB)] = nnGroups
+
+        return Trans2NNGroups
+
