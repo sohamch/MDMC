5d8d04d47f89adfe38873428b9e504bb32b789de
sohamch <ch.soham@gmail.com>
2021-12-26 08:07:21 -0600

    Starting Code dev on New repo

---
+++ MD_KMC/NEB_steps_multiTraj.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+import numpy as np
+import pickle
+import h5py
+import subprocess
+import sys
+import time
+from numba import jit, float64, int64
+from ase.io.lammpsdata import write_lammps_data, read_lammps_data
+from scipy.constants import physical_constants
+
+kB = physical_constants["Boltzmann constant in eV/K"][0]
+
+args = list(sys.argv)
+T = float(args[1])
+Nsteps = int(args[2])
+Nseg = int(args[3])
+NProc = int(args[4])
+NImage = int(args[5])
+ProcPerImage = 1
+
+# if len(args) == 6:
+#     ProcPerImage = 1
+# else:
+#     ProcPerImage = int(args[6])
+
+if NProc%(NImage*ProcPerImage) != 0:
+    raise ValueError("Processors cannot be divided integrally across trajectories")
+
+# Processors per trajectory = NImage*ProcPerImage
+Ntraj = NProc//(NImage*ProcPerImage)
+    
+print("Running {0} steps at {1}K".format(Nsteps, T))
+print("Segments at every {}th step".format(Nseg))
+
+@jit(nopython=True)
+def getJumpSelects(rates):
+    Ntraj = rates.shape[0]
+    timeStep = 1./np.sum(rates, axis=1)
+    ratesProb = rates*timeStep.reshape(Ntraj, 1)
+    ratesProbSum = np.cumsum(ratesProb, axis=1)
+    rn = np.random.rand(Ntraj)
+    jumpID = np.zeros(Ntraj, dtype=int)
+    for tr in range(Ntraj):
+        jSelect = np.searchsorted(ratesProbSum[tr, :], rn[tr])
+        jumpID[tr] = jSelect
+    return jumpID, timeStep
+
+@jit(nopython=True)
+def updateStates(SiteIndToNgb, Nspec, SiteIndToSpec, vacSiteInd, jumpID, dxList):
+    Ntraj = jumpID.shape[0]
+    jumpAtomSelectArray = np.zeros(Ntraj, dtype=int64)
+    X = np.zeros(Ntraj, Nspec, 3)
+    for tr in range(Ntraj):
+        jumpSiteSelect = SiteIndToNgb[vacSiteInd[tr], jumpID[tr]]
+        jumpAtomSelect = SiteIndToSpec[tr, jumpSiteSelect]
+        jumpAtomSelectArray[tr] = jumpAtomSelect
+        SiteIndToSpec[tr, vacSiteInd] = jumpAtomSelect
+        SiteIndToSpec[tr, jumpSiteSelect] = -1 # The next vacancy site
+        vacSiteInd[tr] = jumpSiteSelect
+        X_traj[tr, 0, :] = dxList[jumpID[tr]]
+        X_traj[tr, jumpAtomSelect, :] = -dxList[jumpID[tr]]
+        
+    return jumpAtomSelectArray, X_traj
+
+with open("CrysDat/jnetFCC.pkl", "rb") as fl:
+    jnetFCC = pickle.load(fl)
+dxList = np.array([dx*3.59 for (i, j), dx in jnetFCC[0]])
+print(dxList)
+
+vacSiteInd = 0 # initially the vacancy is at site 0 by design
+
+cmd = [
+    "mpirun -np {0} $LMPPATH/lmp -p {1}x{2} -in in.neb > out.txt".format(NProc, NImage, ProcPerImage)
+]
+
+start = time.time()
+neb_Time = 0.
+neb_count = 0
+
+print("Running {} trajectories on {} processors".format(Ntraj, NProc))
+
+# Load the starting data for the trajectories
+SiteIndToSpec = np.load("SiteIndToSpec.npy") # Ntraj x Nsites array of occupancies
+SiteIndToPos = np.load("SiteIndToLmpCartPos.npy") # lammps pos of sites
+SiteIndToNgb = np.load("siteIndtoNgbSiteInd.npy") # Nsites x z array of site neighbors
+vacSiteInd = np.load("vacSiteInd.npy") # Ntraj size array: contains where the vac is in each traj.
+specs, counts = np.unique(SiteIndToSpec[0], return_counts=True)
+Nspec = len(specs)
+
+try:
+    X_steps = np.load("X_steps.npy")
+    t_steps = np.load("X_steps.npy")
+    stepsLast = np.load("steps_last.npy")[0]
+except:    
+    X_steps = np.zeros((Ntraj, Nspec, Nsteps + 1, 3)) # 0th position will store vacancy jumps
+    t_steps = np.zeros(Ntraj, Nsteps + 1)
+    stepsLast = 0
+    
+stepCount = np.zeros(1, dtype=int)
+
+for step in range(Nsteps - stepsLast):
+    # Write the initial states from last accepted state
+    write_cmd = subprocess.Popen(
+            "mpirun -np {0} python write_init_states.py".format(Ntraj),
+            shell=True
+        )
+    write_cmd.wait()
+    
+    rates = np.zeros(Ntraj, SiteIndToNgb.shape[1])
+    for jumpInd in enumerate(SiteIndToNgb.shape[1]):
+        # Write the final states in NEB format for lammps
+        write_cmd = subprocess.Popen(
+            ["mpirun -np {0} python write_final_state.py {1}".format(Ntraj, jumpInd)],
+            shell=True
+        )
+        write_cmd.wait()
+        
+        # Then run lammps
+        commands = [
+            "mpirun -np {0} $LMPPATH/lmp -p {1}x1 -in in.neb_{2} > out_{2}.txt".format(NProc, NImage, traj)
+            for traj in range(Ntraj)
+        ]
+        cmdList = [subprocess.Popen(cmd, shell=True)]
+        
+        # wait for the lammps commands to complete
+        for c in cmdList:
+            rt_code = c.wait()
+            assert rt_code == 0 # check for system errors
+        
+        # Then read the forward barrier -> ebf
+        for traj in range(Ntraj):
+            with open("out_{0}.txt".format(traj), "r") as fl:
+                for line in fl:
+                    continue
+            ebfLine = line.split()
+            ebf = float(ebfLine[6])
+            rates[traj, jumpInd] = np.exp(-ebf/(kB*T))
+    
+    # Then do selection 
+    jumpID, time_step = getJumpSelects(rates)
+    
+    # Then do the final exchange
+    jumpAtomSelectArray, X_traj = updateStates(SiteIndToNgb, SiteIndToSpec, vacSiteInd, jumpID, dxList)
+        
+    # Note the displacements and the time
+    X_steps[:, :, step + stepLast + 1, :] = X_traj[:, :, :]
+    t_steps[step + stepLast + 1] = time_step
+    stepCount[0] = step + stepLast
+    
+    # save arrays for next step
+    np.save("SiteIndToSpec.npy", SiteIndToSpec)
+    np.save("vacSiteInd.npy", vacSiteInd)
+    np.save("X_steps.npy", X_steps)
+    np.save("t_steps.npy", t_steps)
+    np.save("steps_last.npy", stepCount)
---
+++ MD_MC/Init_state_MC.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+import subprocess
+import pickle
+import time
+from ase.spacegroup import crystal
+from ase.build import make_supercell
+from ase.io.lammpsdata import write_lammps_data, read_lammps_data
+
+
+# In[ ]:
+
+
+from scipy.constants import physical_constants
+kB = physical_constants["Boltzmann constant in eV/K"][0]
+
+
+# In[ ]:
+
+
+import sys
+args = list(sys.argv)
+T = float(args[1])
+N_therm = int(args[2]) # thermalization steps
+N_swaps = int(args[3]) # intervals to draw samples from after thermalization
+N_units = int(args[4]) # dimensions of unit cell
+N_proc = int(args[5]) # No. of procs to parallelize over
+N_samples = int(args[6]) # How many samples we want to draw from this run
+jobID = int(args[7])
+
+
+# In[ ]:
+
+
+# Create an FCC primitive unit cell
+a = 3.59
+fcc = crystal('Ni', [(0,0,0)], spacegroup=225, cellpar=[a, a, a, 90, 90, 90], primitive_cell=True)
+
+# Form a supercell with a vacancy at the centre
+N_units = 8
+superlatt = np.identity(3)*N_units
+superFCC = make_supercell(fcc, superlatt)
+Nsites = len(superFCC.get_positions())
+print(Nsites)
+# randomize occupancies of the sites
+Nperm = 10
+Indices = np.arange(Nsites)
+for i in range(Nperm):
+    Indices = np.random.permutation(Indices)
+
+NSpec = 5
+partition = Nsites//NSpec
+
+elems = ["Co", "Ni", "Cr", "Fe", "Mn"]
+elemsToNum = {}
+for elemInd, el in enumerate(elems):
+    elemsToNum[el] = elemInd + 1
+    
+for i in range(NSpec):
+    for at_Ind in range(i*partition, (i+1)*partition):
+        permInd = Indices[at_Ind]
+        superFCC[permInd].symbol = elems[i]
+del(superFCC[0])
+Natoms = len(superFCC)
+
+# save the supercell: will be useful for getting site positions
+with open("superInitial_{}.pkl".format(jobID),"wb") as fl:
+    pickle.dump(superFCC, fl)
+# Write the supercell as a lammps file
+write_lammps_data("lammpsCoords.txt", superFCC, specorder=elems)
+
+# Next, we write the MC loop
+def MC_Run(SwapRun, ASE_super, Nprocs, slurm_cmd=True):
+    cmdString = "mpirun -np {0} $LMPPATH/lmp -in in_{1}.minim > out_{1}.txt".format(Nprocs,jobID) 
+    for swap in range(SwapRun):
+        # write the supercell as a lammps file
+        write_lammps_data("inp_MC_{0}.data".format(jobID), ASE_Super, specorder=elems)
+
+        # evaluate the energy
+        cmd = subprocess.Popen(cmdString, shell=True)
+        rt = cmd.wait()
+        assert rt == 0
+        
+        # read the energy
+        with open("Eng_{0}.txt".format(jobID), "r") as fl:
+            e1 = fl.readline().split()[0]
+            e1 = float(e1)
+
+        # Now randomize the atomic occupancies
+        site1 = np.random.randint(0, Natoms)
+        site2 = np.random.randint(0, Natoms)
+        while site1 == site2:
+            site1 = np.random.randint(0, Natoms)
+            site2 = np.random.randint(0, Natoms)
+
+        # change the occupancies
+        tmp = ASE_Super[site1].symbol
+        ASE_Super[site1].symbol = ASE_Super[site2].symbol
+        ASE_Super[site2].symbol = tmp
+
+        # write the supercell again as a lammps file
+        write_lammps_data("inp_MC_{0}.data".format(jobID), ASE_Super, specorder=elems)
+
+        # evaluate the energy
+        cmd = subprocess.Popen(cmdString, shell=True)
+        rt = cmd.wait()
+        assert rt == 0
+        # read the energy
+        with open("Eng_{0}.txt".format(jobID), "r") as fl:
+            e2 = fl.readline().split()[0]
+            e2 = float(e2)
+
+        # make decision
+        de = e2 - e1
+        if np.random.rand() < np.exp(-de/(kB*T)):
+            # Then accept the move
+            N_accept += 1
+            continue
+        else:
+            # reject the move by reverting the occupancies
+            tmp = ASE_Super[site1].symbol
+            ASE_Super[site1].symbol = ASE_Super[site2].symbol
+            ASE_Super[site2].symbol = tmp
+    return N_accept
+
+# First thermalize the starting state
+N_accept = MC_Run(N_therm, superFCC, N_proc)
+print("Thermalization Run acceptance ratio : {}".format(N_accept))
+
+occs = np.zeros((Nsamples, Nsites), dtype=np.int16)
+occs[:, 0] = -1
+accept_ratios = np.zeros(Nsamples)
+# Now draw samples
+start = time.time()
+for smp in range(Nsamples):    
+    # Update the state
+    N_accept = MC_Run(N_swaps, superFCC)
+    accept_ratios[smp] = (1.0*N_accept)/N_swaps
+    # store the occupancies
+    for at in superFCC:
+        idx = at.index
+        occs[smp, idx+1] = elemsToNum[at.symbol]
+end = time.time()
+np.save("Occs_{0}.npy".format(jobID))
+print("{} samples drawn with {} swaps. Time: {:.4f} minutes".format(Nsamples, N_swaps, (end-start)/60.))
