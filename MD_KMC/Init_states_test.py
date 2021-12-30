48dddb19c9239cb8d2ea612c71b53a6c414b41da
sohamch <ch.soham@gmail.com>
2021-12-30 10:52:02 -0600

    Adding code for random test generation.

---
+++ MD_KMC/Init_states_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sys
+args = list(sys.argv)
+N_units = int(args[1])
+NSpec = int(args[2])
+Ntraj = int(args[3])
+
+Nsites = N_units * N_units * N_units
+
+SiteIndToSpec = np.zeros((Ntraj, Nsites), dtype=int)
+
+for traj in range(Ntraj):
+    SiteIndToSpec[traj, 0] = -1
+    SiteIndToSpec[traj, 1:] = np.random.randint(1, NSpec+1, Nsites-1)[:]
+    SiteIndToSpec[traj, :] = np.random.permutation(SiteIndToSpec[traj, :])
+
+np.save("SiteIndToSpec.npy", SiteIndToSpec)
