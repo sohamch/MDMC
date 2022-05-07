804a6da04bc5ba95b9ef708406b2d96bf9a3a49c
Soham Chattopadhyay <sohamc2@hal-login2.ncsa.illinois.edu>
2022-05-07 11:16:49 -0500

    fixing missing module init

--- ML_runs/GCNetRun.py
+++ ML_runs/GCNetRun.py
@@ -107,6 +107,8 @@ class GCSubNet(nn.Module):
 class GCSubNetRes(nn.Module):
     def __init__(self, GnnPerms, gdiags, NNsites, SitesToShells,
                 dim, N_ngb, NSpec=5, specsToTrain=[5], mean=0.0, std=0.1, b=1.0, nl=3, nch=8):
+        
+        super().__init__()
 
         NspecsTrain = len(specsToTrain)
         NsubNets = NSpec - NspecsTrain
