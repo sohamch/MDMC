ff77e4d3e6b3d019fa9d1e356b6e955f18f94bbe
Soham Chattopadhyay <sohamc2@hal-login2.ncsa.illinois.edu>
2022-05-11 23:27:27 -0500

    Added upto expansion function.

--- VKMC/HEA_ClusExp/HEA_LBAM.py
+++ VKMC/HEA_ClusExp/HEA_LBAM.py
@@ -2,7 +2,11 @@
 # coding: utf-8
 
 import sys
+# This is the path to the cluster expansion modules
 sys.path.append("/home/sohamc2/HEA_FCC/MDMC/VKMC/")
+
+# This is the path to the GCNetRun module - from which data loaders will be used
+sys.path.append("/home/sohamc2/HEA_FCC/MDMC/ML_runs/")
 import os
 RunPath = os.getcwd() + "/"
 
@@ -18,170 +22,129 @@ import pickle
 import h5py
 from tqdm import tqdm
 from numba import jit, int64, float64
+from GCNetRun import Load_Data 
+import gc
 
-# ## Set up the vector cluster expander with the saved supercell
-import sys
-args = list(sys.argv)
-T = int(args[1])
-MaxOrder = int(args[2])
-clustCut = float(args[3])
-SpecExpand = int(args[4])
-
-# Load the data set
-print("Loading dataset")
-with h5py.File(DataPath+"singleStep_{0}.h5".format(T), "r") as fl:
-    perm = np.array(fl["Permutation"])
-    state1List = np.array(fl["InitStates"])[perm]
-    state2List = np.array(fl["FinStates"])[perm]
-    AllJumpRates = np.array(fl["AllJumpRates"])[perm]
-    rateList = np.array(fl["rates"])[perm]
-    dispList = np.array(fl["SpecDisps"])[perm]
-    jumpSelects = np.array(fl["JumpSelects"])[perm].astype(np.int8)
-
-AllSpecs = np.unique(state1List[0])
-NSpec = AllSpecs.shape[0]
-Nsamples = state1List.shape[0]
-SpecExpand = NSpec - 1 - SpecExpand
 
 N_units = 8 # No. of unit cells along each axis in the supercell
             # The HEA simulations were all done on 8x8x8 supercells
             # So we restrict ourselves to that
 
 # Load all the crystal data
-print("Loading FCC Crystal data")
-a0 = 3.59
-jList = np.load(CrysDatPath+"CrysDat_FCC/jList.npy")
-dxList = np.load(CrysDatPath+"CrysDat_FCC/dxList.npy") * a0
-RtoSiteInd = np.load(CrysDatPath+"CrysDat_FCC/RtoSiteInd.npy")
-siteIndtoR = np.load(CrysDatPath+"CrysDat_FCC/SiteIndtoR.npy")
-jumpNewIndices = np.load(CrysDatPath+"CrysDat_FCC/JumpNewSiteIndices.npy")
+def Load_crys_Data(typ="FCC")
+    print("Loading {} Crystal data".format(typ))
+    jList = np.load(CrysDatPath+"CrysDat_{}/jList.npy".format(typ))
+    dxList = np.load(CrysDatPath+"CrysDat_{}/dxList.npy".format(typ))
+    jumpNewIndices = np.load(CrysDatPath+"CrysDat_{}/JumpNewSiteIndices.npy".format(typ))
 
-with open(CrysDatPath+"CrysDat_FCC/supercellFCC.pkl", "rb") as fl:
-    superFCC = pickle.load(fl)
-crys = superFCC.crys
+    with open(CrysDatPath+"CrysDat_{}/supercellFCC.pkl".format(typ), "rb") as fl:
+        superFCC = pickle.load(fl)
 
-with open(CrysDatPath+"CrysDat_FCC/jnetFCC.pkl", "rb") as fl:
-    jnetFCC = pickle.load(fl)
+    with open(CrysDatPath+"CrysDat_{}/jnetFCC.pkl".format(typ), "rb") as fl:
+        jnetFCC = pickle.load(fl)
 
-vacsite = cluster.ClusterSite((0, 0), np.zeros(3, dtype=int))
-vacsiteInd = superFCC.index(np.zeros(3, dtype=int), (0, 0))[0]
-assert vacsiteInd == 0
+    vacsite = cluster.ClusterSite((0, 0), np.zeros(3, dtype=int))
+    vacsiteInd = superFCC.index(np.zeros(3, dtype=int), (0, 0))[0]
+    assert vacsiteInd == 0
+    return jList, dxList, jumpNewIndices, superFCC, jnetFCC, vacsite, vacsiteInd
 
-TScombShellRange = 1  # upto 1nn combined shell
-TSnnRange = 4
-TScutoff = np.sqrt(2)  # 4th nn cutoff - must be the same as TSnnRange
+def makeVClusExp(superCell, clustCut, MaxOrder, NSpec, vacsite)
+    TScombShellRange = 1  # upto 1nn combined shell
+    TSnnRange = 4
+    TScutoff = np.sqrt(2)  # 4th nn cutoff - must be the same as TSnnRange
 
 
-print("Creating cluster expansion.")
-clusexp = cluster.makeclusters(crys, clustCut, MaxOrder)
+    print("Creating cluster expansion.")
+    crys = superCell.crys
+    clusexp = cluster.makeclusters(crys, clustCut, MaxOrder)
 
-# We'll create a dummy KRA expander anyway since the MC_JIT module is designed to accept transition arrays
-# However, this dummy KEA expander will never get used
-VclusExp = Cluster_Expansion.VectorClusterExpansion(superFCC, clusexp, NSpec, vacsite, MaxOrder, TclusExp=True,
+    # We'll create a dummy KRA expander anyway since the MC_JIT module is designed to accept transition arrays
+    # However, this dummy KEA expander will never get used
+    VclusExp = Cluster_Expansion.VectorClusterExpansion(superFCC, clusexp, NSpec, vacsite, MaxOrder, TclusExp=True,
                                                     TScutoff=TScutoff, TScombShellRange=TScombShellRange,
                                                     TSnnRange=TSnnRange, jumpnetwork=jnetFCC,
                                                     OrigVac=False, zeroClusts=True)
 
-print("generating interaction and vector basis data.")
-VclusExp.generateSiteSpecInteracts()
-# Generate the basis vectors for the clusters
-VclusExp.genVecClustBasis(VclusExp.SpecClusters)
-VclusExp.indexVclus2Clus()  # Index vector cluster list to cluster symmetry groups
-VclusExp.indexClustertoVecClus()
-NVclus = len(VclusExp.vecVec)
+    print("generating interaction and vector basis data.")
+    VclusExp.generateSiteSpecInteracts()
+    # Generate the basis vectors for the clusters
+    VclusExp.genVecClustBasis(VclusExp.SpecClusters)
+    VclusExp.indexVclus2Clus()  # Index vector cluster list to cluster symmetry groups
+    VclusExp.indexClustertoVecClus()
 
-# First, we have to generate all the arrays
-# Lattice gas Like -  set all energies to zero
-# All the rates are known to us anyway - they are the ones that are going to get used
-Energies = np.zeros(len(VclusExp.SpecClusters))
-KRAEnergies = [np.zeros(len(KRAClusterDict)) for (key, KRAClusterDict) in
-               VclusExp.KRAexpander.clusterSpeciesJumps.items()]
+    return VclusExp
 
-# First, the chemical data
-numSitesInteracts, SupSitesInteracts, SpecOnInteractSites,\
-Interaction2En, numInteractsSiteSpec, SiteSpecInterArray = VclusExp.makeJitInteractionsData(Energies)
+def CreateJitCalculator(VclusExp):
+    # First, we have to generate all the arrays
+    # Lattice gas Like -  set all energies to zero
+    # All the rates are known to us anyway - they are the ones that are going to get used
+    Energies = np.zeros(len(VclusExp.SpecClusters))
+    KRAEnergies = [np.zeros(len(KRAClusterDict)) for (key, KRAClusterDict) in 
+            VclusExp.KRAexpander.clusterSpeciesJumps.items()]
 
-# Next, the vector basis data
-numVecsInteracts, VecsInteracts, VecGroupInteracts = VclusExp.makeJitVectorBasisData()
+    # First, the chemical data
+    numSitesInteracts, SupSitesInteracts, SpecOnInteractSites,\
+    Interaction2En, numInteractsSiteSpec, SiteSpecInterArray = VclusExp.makeJitInteractionsData(Energies)
 
-# Note : The KRA expansion works only for binary alloys
-# Right now we don't need them, since we already know the rates
-# However, we create a dummy one since the JIT MC calculator requires the arrays
-KRACounterSpec = 1
-TsInteractIndexDict, Index2TSinteractDict, numSitesTSInteracts, TSInteractSites,\
-TSInteractSpecs, jumpFinSites, jumpFinSpec, FinSiteFinSpecJumpInd, numJumpPointGroups,\
-numTSInteractsInPtGroups, JumpInteracts, Jump2KRAEng = VclusExp.KRAexpander.makeTransJitData(KRACounterSpec, KRAEnergies)
+    # Next, the vector basis data
+    numVecsInteracts, VecsInteracts, VecGroupInteracts = VclusExp.makeJitVectorBasisData()
 
-# Make the MC class
-KRASpecConstants = np.random.rand(NSpec-1)
-MCJit = MC_JIT.MCSamplerClass(
-    numSitesInteracts, SupSitesInteracts, SpecOnInteractSites, Interaction2En,
-    numInteractsSiteSpec, SiteSpecInterArray,
-    numSitesTSInteracts, TSInteractSites, TSInteractSpecs, jumpFinSites, jumpFinSpec,
-    FinSiteFinSpecJumpInd, numJumpPointGroups, numTSInteractsInPtGroups,
-    JumpInteracts, Jump2KRAEng, KRASpecConstants
-)
+    # Note : The KRA expansion works only for binary alloys
+    # Right now we don't need them, since we already know the rates
+    # However, we create a dummy one since the JIT MC calculator requires the arrays
+    KRACounterSpec = 1
+    TsInteractIndexDict, Index2TSinteractDict, numSitesTSInteracts, TSInteractSites,\
+    TSInteractSpecs, jumpFinSites, jumpFinSpec, FinSiteFinSpecJumpInd, numJumpPointGroups,\
+    numTSInteractsInPtGroups, JumpInteracts, Jump2KRAEng = VclusExp.KRAexpander.makeTransJitData(KRACounterSpec, KRAEnergies)
 
-# Verify that the displacements, selected jumps and state changes are consistent
-print("Verifying displacements")
-for samp in tqdm(range(Nsamples), position=0, leave=True):
-    jSelect = jumpSelects[samp]
-    dispVac = dispList[samp, 0, :]
-    dxJump = dxList[jSelect]
-    assert np.allclose(dxJump, dispVac)
-    state1 = state1List[samp]
-    state2 = state2List[samp]
+    # Make the MC class
+    KRASpecConstants = np.random.rand(NSpec-1)
+    MCJit = MC_JIT.MCSamplerClass(
+        numSitesInteracts, SupSitesInteracts, SpecOnInteractSites, Interaction2En,
+        numInteractsSiteSpec, SiteSpecInterArray,
+        numSitesTSInteracts, TSInteractSites, TSInteractSpecs, jumpFinSites, jumpFinSpec,
+        FinSiteFinSpecJumpInd, numJumpPointGroups, numTSInteractsInPtGroups,
+        JumpInteracts, Jump2KRAEng, KRASpecConstants
+    )
+
+    return MCJit, numVecsInteracts, VecsInteracts, VecGroupInteracts
+
+
+def Expand(state1List, dxList, SpecExpand, AllJumpRates, MCJit, NVclus,
+        numVecsInteracts, VecsInteracts, VecGroupInteracts):
+
+    # Get a dummy TS offsite counts
+    TSOffSc = np.zeros(MCJit.numSitesTSInteracts.shape[0], dtype=np.int8)
+
+    # Then we write the expansion loop
+    totalW = np.zeros((NVclus, NVclus))
+    totalB = np.zeros(NVclus)
+
+    print("Calculating rate and velocity expansions")
+    for samp in tqdm(range(Nsamples//2), position=0, leave=True):
     
-    specJump = state1[jList[jSelect]]
-    assert np.allclose(dispList[samp, specJump, :], -dxJump)
+        # In the cluster expander, the vacancy is the highest labelled species,
+        # In our case, it is the lowest
+        # So we'll change the numbering so that the vacancy is labelled 5
+        state = state1List[samp]
     
-    assert np.array_equal(state2, state1[jumpNewIndices[jSelect]])
-
-# test the offsite counting
-print("Verifying active cluster counting")
-state_test = NSpec - 1 - state1List[0]
-offsc = MC_JIT.GetOffSite(state_test, numSitesInteracts, SupSitesInteracts, SpecOnInteractSites)
-offCounts = np.zeros_like(offsc)
-for interactInd in tqdm(range(numSitesInteracts.shape[0]//10), position=0, leave=True):
-    off_count_interact = 0
-    for siteInd in range(numSitesInteracts[interactInd]):
-        supSite = SupSitesInteracts[interactInd, siteInd]
-        spec = SpecOnInteractSites[interactInd, siteInd]
-        if state_test[supSite] != spec:
-            off_count_interact += 1
-    offCounts[interactInd] = off_count_interact
-    assert off_count_interact == offsc[interactInd]
-
-# Get a dummy TS offsite counts
-TSOffSc = np.zeros(numSitesTSInteracts.shape[0], dtype=np.int8)
-
-# Then we write the expansion loop
-totalW = np.zeros((NVclus, NVclus))
-totalB = np.zeros(NVclus)
-
-print("Calculating rate and velocity expansions")
-for samp in tqdm(range(Nsamples//2), position=0, leave=True):
+        offsc = MC_JIT.GetOffSite(state, MCJit.numSitesInteracts, MCJit.SupSitesInteracts, MCJit.SpecOnInteractSites)
     
-    # In the cluster expander, the vacancy is the highest labelled species,
-    # In our case, it is the lowest
-    # So we'll change the numbering so that the vacancy is labelled 5
-    state = NSpec - 1 - state1List[samp]
-    
-    offsc = MC_JIT.GetOffSite(state, numSitesInteracts, SupSitesInteracts, SpecOnInteractSites)
-    
-    WBar, bBar, rates_used , _, _ = MCJit.Expand(state, jList, dxList, SpecExpand, offsc,
+        WBar, bBar, rates_used , _, _ = MCJit.Expand(state, jList, dxList, SpecExpand, offsc,
                                           TSOffSc, numVecsInteracts, VecGroupInteracts, VecsInteracts,
                                           NVclus, 0, vacsiteInd, AllJumpRates[samp])
     
-    assert np.allclose(rates_used, AllJumpRates[samp])
-    totalW += WBar
-    totalB += bBar
+        assert np.allclose(rates_used, AllJumpRates[samp])
+        totalW += WBar
+        totalB += bBar
 
-totalW /= Nsamples
-totalB /= Nsamples
+    totalW /= Nsamples
+    totalB /= Nsamples
 
-np.save(RunPath + "Wbar_{}.npy".format(T), totalW)
-np.save(RunPath + "Bbar_{}.npy".format(T), totalB)
+    np.save(RunPath + "Wbar_{}.npy".format(T), totalW)
+    np.save(RunPath + "Bbar_{}.npy".format(T), totalB)
+
+    return totalW, totalB
 
 # verify symmetry
 for i in tqdm(range(totalW.shape[0]), position=0, leave=True):
@@ -256,11 +219,51 @@ np.save(RunPath + "L{0}{0}_{1}.npy".format(SpecExpand, T), np.array([L_train, L_
 
 if __name__ == "__main__":
 
+    args = list(sys.argv)
+    count = 1
+    T = int(args[count])
+    count += 1
+
+    FileName = args[count]
+    count += 1
+
+    MaxOrder = int(args[count])
+    count += 1
+
+    clustCut = float(args[count])
+    count += 1
+
+    SpecExpand = int(args[count])
+    count += 1
+
+    VacSpec = int(args[count])
+
+
     # Load Data
+    state1List, state2List, dispList, rateList, AllJumpRates = Load_Data(FileName)
+    AllSpecs = np.unique(state1List[0])
+    NSpec = AllSpecs.shape[0]
+    Nsamples = state1List.shape[0]
+
+    if VacSpec == 0:
+        # Convert data so that vacancy is highest 
+        state1List = NSpec - 1 - state1List
+        state2List = NSpec - 1 - state2List
+        dispListNew = np.zeros_like(dispList)
+        for spec in range(NSpec):
+            dispListNew[:, NSpec - 1 - spec, :] = dispList[:, spec, :]
+        del(dispList)
+        gc.collect()
+
+        dispList = dispListNew
+
+        SpecExpand = NSpec - 1 - SpecExpand
+
 
     # Load Crystal Data
 
     # Load vecClus - if existing, else create new
+    NVclus = len(VclusExp.vecVec)
 
     # Make MCJIT
 
