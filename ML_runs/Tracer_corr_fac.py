76909c3e0a4fcdb274154c291f73547d09e7b28f
Soham Chattopadhyay <sohamc2@hal-login2.ncsa.illinois.edu>
2022-09-02 06:25:35 -0500

    fixing name error

--- ML_runs/Tracer_corr_fac.py
+++ ML_runs/Tracer_corr_fac.py
@@ -109,7 +109,7 @@ def main(args):
     hostState = hostState.to(device)
 
     gNet = GCNet(GnnPerms.long(), NNsites, JumpUnitVecs, dim=3, N_ngb=N_ngb,
-                 NSpec=1, mean=0.0, std=0.2, nl=args.Nlayers, nch=args.NChannels, nchLast=1).double()
+                 NSpec=1, mean=0.0, std=0.2, nl=args.NLayers, nch=args.NChannels, nchLast=1).double()
 
     if args.StartEpoch > 0:
         gNet.load_state_dict(pt.load(RunPath + "epochs_tracer_{0}_16_Sup/ep_{1}.pt".format(args.CrysType, epoch_last),map_location="cpu"))
