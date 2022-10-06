d343d4d53f78390c7fa4ce3a93565dc2d9c36aa4
sohamch <ch.soham@gmail.com>
2022-10-06 06:17:01 -0500

    Adding symmetry check for y vectors. All test passed.

--- Symm_Network/test_SymmLayers.py
+++ Symm_Network/test_SymmLayers.py
@@ -120,3 +120,46 @@ class TestGConv(unittest.TestCase):
                             outsamp[siteIndNew] = outsamp0[siteInd]
                         self.assertTrue(pt.allclose(outsamp, out[gInd, ch, :], atol=1e-12))
                 print("Layer {} symmetry assertion passed".format((l + 3) // 3))
+
+    def test_yVecs(self):
+        # Check the y vectors explicitly
+        gNet = GCNet(self.GnnPerms.long(), self.NNsites, self.JumpVecs, N_ngb=self.N_ngb, NSpec=self.NspCh,
+                     mean=0.02, std=0.2, nl=2, nch=8, nchLast=5).double()
+        with pt.no_grad():
+            y = gNet(self.StateTensors)
+
+        y_np = y.data.numpy().copy()
+        mn = np.min(y_np.flatten())
+        mx = np.max(y_np.flatten())
+        av = np.mean(y_np.flatten())
+        print("min : {}, max : {}, mean: {}".format(mn, mx, av))
+
+        # First, check the y vectors explicitly
+        with pt.no_grad():
+            out = gNet.net(self.StateTensors)
+            for stateInd in tqdm(range(out.shape[0]), position=0, leave=True, ncols=65):
+                for channel in range(out.shape[1]):
+                    for site in range(self.Nsites):
+                        _, Rsite = self.superFCC.ciR(site)
+                        a_site = pt.zeros(self.z, dtype=pt.double)
+                        for jmp in range(self.z):
+                            nnJump, ci = self.superFCC.crys.cart2pos(self.dxJumps[jmp])
+                            self.assertEqual(ci, (0, 0))
+                            RsiteNew = Rsite + nnJump
+                            siteNew, _ = self.superFCC.index(RsiteNew, (0, 0))
+                            self.assertEqual(siteNew, self.NNsites[jmp + 1, site])
+                            a_site[jmp] = out[stateInd, channel, siteNew]
+
+                        siteVec = sum(a_site[i] * self.JumpVecs[:, i] for i in range(self.z)).detach().numpy()
+                        assert np.allclose(siteVec, y_np[stateInd, channel, :, site])
+
+        # Then check their symmetry
+        y0 = y_np[self.IdentityIndex].copy()
+        for gInd, g in tqdm(self.GIndToGDict.items(), position=0, leave=True, ncols=65):
+            for site in range(512):
+                _, Rsite = self.superFCC.ciR(site)
+                RsiteNew, _ = self.superFCC.crys.g_pos(g, Rsite, (0, 0))
+                RsiteNew = RsiteNew
+                siteNew, _ = self.superFCC.index(RsiteNew, (0, 0))
+                for ch in range(y0.shape[0]):
+                    assert np.allclose(np.dot(g.cartrot, y0[ch, :, site]), y_np[gInd, ch, :, siteNew])
