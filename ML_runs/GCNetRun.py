c52b13e3f67a4db11d597908e9a2d3600acb1d50
sohamch <ch.soham@gmail.com>
2022-10-06 03:21:03 -0500

    Removing unnecessary argument.

--- ML_runs/GCNetRun.py
+++ ML_runs/GCNetRun.py
@@ -270,7 +270,7 @@ def sort_jp(jProbs_st1, jProbs_st2, jumpSort):
 
 """## Write the training loop"""
 def Train(T, dirPath, State1_Occs, State2_Occs, OnSites_st1, OnSites_st2, rates, disps,
-          jProbs_st1, jProbs_st2, NNsites, SpecsToTrain, sp_ch, VacSpec, start_ep, end_ep, interval, N_train,
+          jProbs_st1, jProbs_st2, SpecsToTrain, sp_ch, VacSpec, start_ep, end_ep, interval, N_train,
           gNet, lRate=0.001, batch_size=128, scratch_if_no_init=True, DPr=False, Boundary_train=False, jumpSort=True,
           AddOnSites=True, scaleL0=False, chkpt=True):
     
