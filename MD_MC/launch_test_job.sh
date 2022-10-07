f13227b12061f3076f92741929b5115f4c050236
sohamch <ch.soham@gmail.com>
2022-10-07 17:10:38 -0500

    Updating tests with re-rurns, coordinate transformation etc.

--- MD_MC/launch_test_job.sh
+++ MD_MC/launch_test_job.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-mkdir test_1
-cd test_1
-python3 ../Init_state_MC.py 1073 100 8 1 $i 1 1 1 0
+mkdir test_MC_traj
+cd test_MC_traj
+
+mkdir 1
+cd 1
+python3 ../../Init_state_MC.py 1073 100 8 1 1 1 1 1
+
+# remove the last 10 checkpoint files to simulate incomplete run
+for i in {90..100}
+do
+	rm chkpt/supercell_${i}.pkl
+done
+
+# copy the lammps command file before continuation runs
+# we'll compare the two command files later to ensure
+# no new command files were created and that the
+# same random seed was used to displace atoms in both the previous
+# and the continuation runs
+cp in_1.minim in_1_run_1.minim
+
+# Now re-run from last checkpoint
+python3 ../../Init_state_MC.py 1073 100 8 1 1 1 1 1 1
