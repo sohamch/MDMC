ee0f56f66fc2826cba1278178c08cd9e77ab58b3
sohamch <ch.soham@gmail.com>
2023-01-14 10:08:11 -0600

    Trying to modularize KMC launch script. Test functions are untouched.

--- MD_KMC/NEB_steps_multiTraj.py
+++ MD_KMC/NEB_steps_multiTraj.py
@@ -29,8 +29,8 @@ MainPath = args[8] # The path where the potential file is found
 CrysDatPath = args[9] # The path where the Crystal data is found
 WriteAllJumps = bool(int(args[10]))
 
-SourcePath = os.path.split(os.path.realpath(__file__))[0] # the directory where the main script is
-SourcePath += "/"
+# SourcePath = os.path.split(os.path.realpath(__file__))[0] # the directory where the main script is
+# SourcePath += "/"
 
 # Need to get rid of these argument
 NImage = 3
@@ -54,172 +54,175 @@ def CreateLammpsData(N_units):
     SiteIndToCartPos = np.zeros((Nsites, 3))
     for i in range(Nsites):
         SiteIndToCartPos[i, :] = Sup_lammps_unrelax_coords[i].position[:]
-    np.save("SiteIndToLmpCartPos.npy", SiteIndToCartPos)
+    np.save("SiteIndToLmpCartPos.npy", SiteIndToCartPos) # In case we want to check later
     return SiteIndToCartPos
 
-# Load the lammps cartesian positions and neighborhoods
-SiteIndToPos = CreateLammpsData(Nunits) # np.load(SourcePath + "SiteIndToLmpCartPos.npy")  # lammps pos of sites
+def Load_crysDat(CrysDatPath, a0):
+    with h5py.File(CrysDatPath, "r") as fl:
+        dxList = np.array(fl["dxList_1nn"])
+        SiteIndToNgb = np.array(fl["NNsiteList_sitewise"])[1:, :].T
 
-with h5py.File(CrysDatPath, "r") as fl:
-    dxList = np.array(fl["dxList_1nn"])
-    SiteIndToNgb = np.array(fl["NNsiteList_sitewise"])[1:, :].T
-
-assert SiteIndToNgb.shape[0] == SiteIndToPos.shape[0]
-assert SiteIndToNgb.shape[1] == dxList.shape[0]
-
-dxList *= 3.59
+    assert SiteIndToNgb.shape[1] == dxList.shape[0]
+    dxList *= a0
+    return dxList, SiteIndToNgb
 
 # load the data
-if startStep > 0:
-    
-    with h5py.File(RunPath + "data_{0}_{1}_{2}.h5".format(T, startStep, StateStart), "r") as fl:
-        batchStates = np.array(fl["FinalStates"])
-    
-    try: 
-        assert batchStates.shape[0] == batchSize
-    except AssertionError:
-        raise AssertionError("The checkpointed number of states does not match batchSize argument.")
+def load_Data():
+    if startStep > 0:
 
-    SiteIndToSpecAll = batchStates
+        with h5py.File(RunPath + "data_{0}_{1}_{2}.h5".format(T, startStep, StateStart), "r") as fl:
+            batchStates = np.array(fl["FinalStates"])
 
-    vacSiteIndAll = np.zeros(batchSize, dtype=int)
-    for stateInd in range(SiteIndToSpecAll.shape[0]):
-        state = SiteIndToSpecAll[stateInd]
-        vacSite = np.where(state == 0)[0][0]
-        vacSiteIndAll[stateInd] = vacSite
+        try:
+            assert batchStates.shape[0] == batchSize
+        except AssertionError:
+            raise AssertionError("The checkpointed number of states does not match batchSize argument.")
 
-    print("Starting from checkpointed step {}".format(startStep))
-    np.save("states_{0}_{1}_{2}.npy".format(T, startStep, StateStart), SiteIndToSpecAll)
-    np.save("vacSites_{0}_{1}_{2}.npy".format(T, startStep, StateStart), vacSiteIndAll)
+        SiteIndToSpecAll = batchStates
 
-else:
-    assert startStep == 0
-    InitStateFile = args[11]
+        vacSiteIndAll = np.zeros(batchSize, dtype=int)
+        for stateInd in range(SiteIndToSpecAll.shape[0]):
+            state = SiteIndToSpecAll[stateInd]
+            vacSite = np.where(state == 0)[0][0]
+            vacSiteIndAll[stateInd] = vacSite
+
+        print("Starting from checkpointed step {}".format(startStep))
+        np.save("states_{0}_{1}_{2}.npy".format(T, startStep, StateStart), SiteIndToSpecAll)
+        np.save("vacSites_{0}_{1}_{2}.npy".format(T, startStep, StateStart), vacSiteIndAll)
+
+    else:
+        assert startStep == 0
+        InitStateFile = args[11]
+        try:
+            allStates = np.load(InitStateFile)
+            print("Starting from step zero.")
+        except:
+            raise FileNotFoundError("Initial states not found.")
+
+        SiteIndToSpecAll = allStates[StateStart: StateStart + batchSize]
+
+        assert np.all(SiteIndToSpecAll[:, 0] == 0), "All vacancies must be at the 0th site initially."
+        vacSiteIndAll = np.zeros(SiteIndToSpecAll.shape[0], dtype = int)
+        np.save("states_step0_{}.npy".format(T), SiteIndToSpecAll)
+
+    return SiteIndToSpecAll, vacSiteIndAll
+
+def DoKMC(SiteIndToSpecAll, vacSiteIndAll):
     try:
-        allStates = np.load(InitStateFile)
-        print("Starting from step zero.")
+        with open("lammpsBox.txt", "r") as fl:
+            Initlines = fl.readlines()
     except:
-        raise FileNotFoundError("Initial states not found.")
+        raise FileNotFoundError("Template lammps data file not found. Run the \"Setup_Lammps_coords.py\" file.")
 
-    SiteIndToSpecAll = allStates[StateStart: StateStart + batchSize]
+    assert SiteIndToSpecAll.shape[1] == len(Initlines[12:])
 
-    assert np.all(SiteIndToSpecAll[:, 0] == 0), "All vacancies must be at the 0th site initially."
-    vacSiteIndAll = np.zeros(SiteIndToSpecAll.shape[0], dtype = int)
-    np.save("states_step0_{}.npy".format(T), SiteIndToSpecAll)
+    specs, counts = np.unique(SiteIndToSpecAll[0], return_counts=True)
+    Nspec = len(specs)  # including the vacancy
+    Ntraj = SiteIndToSpecAll.shape[0]
+    assert Ntraj == batchSize
+    print("No. of samples : {}".format(Ntraj))
 
+    Nsites = SiteIndToSpecAll.shape[1]
+    Initlines[2] = "{} \t atoms\n".format(Nsites - 1)
+    Initlines[3] = "{} atom types\n".format(Nspec - 1)
 
-try:
-    with open("lammpsBox.txt", "r") as fl:
-        Initlines = fl.readlines()
-except:
-    raise FileNotFoundError("Template lammps data file not found. Run the \"Setup_Lammps_coords.py\" file.")
+    # Begin KMC loop below
+    FinalStates = SiteIndToSpecAll
+    FinalVacSites = vacSiteIndAll
+    SpecDisps = np.zeros((Ntraj, Nspec, 3))
+    AllJumpRates = np.zeros((Ntraj, SiteIndToNgb.shape[1]))
+    AllJumpBarriers = np.zeros((Ntraj, SiteIndToNgb.shape[1]))
+    tarr = np.zeros(Ntraj)
+    JumpSelects = np.zeros(Ntraj, dtype=np.int8) # which jump is chosen for each trajectory
+    TestRandomNums = np.zeros(Ntraj) # store the random numbers at all steps
 
-assert SiteIndToSpecAll.shape[1] == len(Initlines[12:])
+    # Before starting, write the lammps input files
+    write_input_files(chunkSize, potPath=MainPath)
 
-specs, counts = np.unique(SiteIndToSpecAll[0], return_counts=True)
-Nspec = len(specs)  # including the vacancy
-Ntraj = SiteIndToSpecAll.shape[0]
-assert Ntraj == batchSize
-print("No. of samples : {}".format(Ntraj))
+    start = time.time()
 
-Nsites = SiteIndToSpecAll.shape[1]
-Initlines[2] = "{} \t atoms\n".format(Nsites - 1)
-Initlines[3] = "{} atom types\n".format(Nspec-1)
+    for step in range(Nsteps):
+        for chunk in range(0, Ntraj, chunkSize):
+            # Write the initial states from last accepted state
+            sampleStart = chunk
+            sampleEnd = min(chunk + chunkSize, Ntraj)
 
-# Begin KMC loop below
-FinalStates = SiteIndToSpecAll
-FinalVacSites = vacSiteIndAll
-SpecDisps = np.zeros((Ntraj, Nspec, 3))
-AllJumpRates = np.zeros((Ntraj, SiteIndToNgb.shape[1]))
-AllJumpBarriers = np.zeros((Ntraj, SiteIndToNgb.shape[1]))
-tarr = np.zeros(Ntraj)
-JumpSelects = np.zeros(Ntraj, dtype=np.int8) # which jump is chosen for each trajectory
-TestRandomNums = np.zeros(Ntraj) # store the random numbers at all steps
+            SiteIndToSpec = FinalStates[sampleStart: sampleEnd].copy()
+            vacSiteInd = FinalVacSites[sampleStart: sampleEnd].copy()
 
-# Before starting, write the lammps input files
-write_input_files(chunkSize, potPath=MainPath)
+            write_init_states(SiteIndToSpec, SiteIndToPos, vacSiteInd, Initlines)
 
-start = time.time()
+            rates = np.zeros((SiteIndToSpec.shape[0], SiteIndToNgb.shape[1]))
+            barriers = np.zeros((SiteIndToSpec.shape[0], SiteIndToNgb.shape[1]))
+            for jumpInd in range(SiteIndToNgb.shape[1]):
+                # Write the final states in NEB format for lammps
+                write_final_states(SiteIndToPos, vacSiteInd, SiteIndToNgb, jumpInd, writeAll=WriteAllJumps)
 
-for step in range(Nsteps):
-    for chunk in range(0, Ntraj, chunkSize):
-        # Write the initial states from last accepted state
-        sampleStart = chunk
-        sampleEnd = min(chunk + chunkSize, Ntraj)
+                # Then run lammps
+                commands = [
+                    "mpirun -np {0} $LMPPATH/lmp -p {0}x1 -in in.neb_{1} > out_{1}.txt".format(NImage, traj)
+                    for traj in range(SiteIndToSpec.shape[0])
+                ]
+                cmdList = [subprocess.Popen(cmd, shell=True) for cmd in commands]
 
-        SiteIndToSpec = FinalStates[sampleStart: sampleEnd].copy()
-        vacSiteInd = FinalVacSites[sampleStart: sampleEnd].copy()
+                # wait for the lammps commands to complete
+                for c in cmdList:
+                    rt_code = c.wait()
+                    assert rt_code == 0  # check for system errors
 
-        write_init_states(SiteIndToSpec, SiteIndToPos, vacSiteInd, Initlines)
+                # Then read the forward barrier -> ebf
+                for traj in range(SiteIndToSpec.shape[0]):
+                    with open("out_{0}.txt".format(traj), "r") as fl:
+                        for line in fl:
+                            continue
+                    ebfLine = line.split()
+                    ebf = float(ebfLine[6])
+                    rates[traj, jumpInd] = np.exp(-ebf / (kB * T))
+                    barriers[traj, jumpInd] = ebf
 
-        rates = np.zeros((SiteIndToSpec.shape[0], SiteIndToNgb.shape[1]))
-        barriers = np.zeros((SiteIndToSpec.shape[0], SiteIndToNgb.shape[1]))
-        for jumpInd in range(SiteIndToNgb.shape[1]):
-            # Write the final states in NEB format for lammps
-            write_final_states(SiteIndToPos, vacSiteInd, SiteIndToNgb, jumpInd, writeAll=WriteAllJumps)
-            
-            # Then run lammps
-            commands = [
-                "mpirun -np {0} $LMPPATH/lmp -p {0}x1 -in in.neb_{1} > out_{1}.txt".format(NImage, traj)
-                for traj in range(SiteIndToSpec.shape[0])
-            ]
-            cmdList = [subprocess.Popen(cmd, shell=True) for cmd in commands]
+                    # get the jumping species and store the barrier for later use
+                    vInd = vacSiteInd[traj]
+                    vacNgb = SiteIndToNgb[vInd, jumpInd]
+                    jAtom = SiteIndToSpec[traj, vacNgb]
 
-            # wait for the lammps commands to complete
-            for c in cmdList:
-                rt_code = c.wait()
-                assert rt_code == 0  # check for system errors
+            # store all the rates
+            AllJumpRates[sampleStart:sampleEnd] = rates[:, :]
+            AllJumpBarriers[sampleStart:sampleEnd] = barriers[:, :]
 
-            # Then read the forward barrier -> ebf
-            for traj in range(SiteIndToSpec.shape[0]):
-                with open("out_{0}.txt".format(traj), "r") as fl:
-                    for line in fl:
-                        continue
-                ebfLine = line.split()
-                ebf = float(ebfLine[6])
-                rates[traj, jumpInd] = np.exp(-ebf / (kB * T))
-                barriers[traj, jumpInd] = ebf
+            # Then do selection
+            jumpID, rateProbs, ratesCsum, rndNums, time_step = getJumpSelects(rates)
+            # store the selected jumps
+            JumpSelects[sampleStart: sampleEnd] = jumpID[:]
 
-                # get the jumping species and store the barrier for later use
-                vInd = vacSiteInd[traj]
-                vacNgb = SiteIndToNgb[vInd, jumpInd]
-                jAtom = SiteIndToSpec[traj, vacNgb]
+            # store the random numbers for testing
+            TestRandomNums[sampleStart: sampleEnd] = rndNums[:]
 
-        # store all the rates
-        AllJumpRates[sampleStart:sampleEnd] = rates[:, :]
-        AllJumpBarriers[sampleStart:sampleEnd] = barriers[:, :]
+            # Then do the final exchange
+            jumpAtomSelectArray, X_traj = updateStates(SiteIndToNgb, Nspec, SiteIndToSpec, vacSiteInd, jumpID, dxList)
+            # def updateStates(SiteIndToNgb, Nspec,  SiteIndToSpec, vacSiteInd, jumpID, dxList):
 
-        # Then do selection
-        jumpID, rateProbs, ratesCsum, rndNums, time_step = getJumpSelects(rates)
-        # store the selected jumps
-        JumpSelects[sampleStart: sampleEnd] = jumpID[:]
+            # save final states, displacements and times
+            FinalStates[sampleStart: sampleEnd, :] = SiteIndToSpec[:, :]
+            FinalVacSites[sampleStart: sampleEnd] = vacSiteInd[:]
+            SpecDisps[sampleStart:sampleEnd, :, :] = X_traj[:, :, :]
+            tarr[sampleStart:sampleEnd] = time_step[:]
+            with open("ChunkTiming.txt", "a") as fl:
+                fl.write(
+                    "Chunk {0} of {1} in step {3} completed in : {2} seconds\n".format(chunk//chunkSize + 1, int(np.ceil(Ntraj/chunkSize)), time.time() - start, step + 1))
 
-        # store the random numbers for testing
-        TestRandomNums[sampleStart: sampleEnd] = rndNums[:]
+        with open("StepTiming.txt", "a") as fl:
+            fl.write("Time per step up to {0} of {1} steps : {2} seconds\n".format(step + 1, Nsteps, (time.time() - start)/(step + 1)))
 
-        # Then do the final exchange
-        jumpAtomSelectArray, X_traj = updateStates(SiteIndToNgb, Nspec, SiteIndToSpec, vacSiteInd, jumpID, dxList)
-        # def updateStates(SiteIndToNgb, Nspec,  SiteIndToSpec, vacSiteInd, jumpID, dxList):
+        # Next, save all the arrays in an hdf5 file for the current step.
+        # For the first 10 steps, store test random numbers.
+        with h5py.File("data_{0}_{1}_{2}.h5".format(T, startStep + step + 1, StateStart), "w") as fl:
+            fl.create_dataset("FinalStates", data=FinalStates)
+            fl.create_dataset("SpecDisps", data=SpecDisps)
+            fl.create_dataset("times", data=tarr)
+            fl.create_dataset("AllJumpRates", data=AllJumpRates)
+            fl.create_dataset("AllJumpBarriers", data=AllJumpBarriers)
+            fl.create_dataset("JumpSelects", data=JumpSelects)
+            fl.create_dataset("TestRandNums", data=TestRandomNums)
 
-        # save final states, displacements and times
-        FinalStates[sampleStart: sampleEnd, :] = SiteIndToSpec[:, :]
-        FinalVacSites[sampleStart: sampleEnd] = vacSiteInd[:]
-        SpecDisps[sampleStart:sampleEnd, :, :] = X_traj[:, :, :]
-        tarr[sampleStart:sampleEnd] = time_step[:]
-        with open("ChunkTiming.txt", "a") as fl:
-            fl.write(
-                "Chunk {0} of {1} in step {3} completed in : {2} seconds\n".format(chunk//chunkSize + 1, int(np.ceil(Ntraj/chunkSize)), time.time() - start, step + 1))
-
-    with open("StepTiming.txt", "a") as fl:
-        fl.write("Time per step up to {0} of {1} steps : {2} seconds\n".format(step + 1, Nsteps, (time.time() - start)/(step + 1)))
-
-    # Next, save all the arrays in an hdf5 file for the current step.
-    # For the first 10 steps, store test random numbers.
-    with h5py.File("data_{0}_{1}_{2}.h5".format(T, startStep + step + 1, StateStart), "w") as fl:
-        fl.create_dataset("FinalStates", data=FinalStates)
-        fl.create_dataset("SpecDisps", data=SpecDisps)
-        fl.create_dataset("times", data=tarr)
-        fl.create_dataset("AllJumpRates", data=AllJumpRates)
-        fl.create_dataset("AllJumpBarriers", data=AllJumpBarriers)
-        fl.create_dataset("JumpSelects", data=JumpSelects)
-        fl.create_dataset("TestRandNums", data=TestRandomNums)
+def main(args):
+    SiteIndToPos = CreateLammpsData(args.Nunits)
