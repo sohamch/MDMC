fccc155a9fbcc2235d897c3015fba0bdc56b1580
sohamch <ch.soham@gmail.com>
2023-01-14 13:47:58 -0600

    Reverting back by removing changes in line 137-140. Previous code was correct.

--- VCE/Cluster_Expansion.py
+++ VCE/Cluster_Expansion.py
@@ -134,11 +134,9 @@ class VectorClusterExpansion(object):
 
             G0 = sum([g.cartrot for g in glist0])/len(glist0)
             vals, vecs = np.linalg.eig(G0)
-            # In order to span R3, the vectors must be real, so we need to consider those
-            # only whose real parts still have an eigenvalue of 1.0
-            vecs = np.real(vecs)
-            vlist = [vecs[:, i]/np.linalg.norm(vecs[:, i]) for i in range(3) if np.isclose(vals[i], 1.0)
-                     and np.allclose(vecs[:, i], np.dot(G0, vecs[:, i]))]
+            vecs = np.real(vecs) # Since G0 is real (cartrot matrices cannot be complex), the
+                                # real part of the eigenvectors with eigval 1.0 will be the invariants.
+            vlist = [vecs[:, i]/np.linalg.norm(vecs[:, i]) for i in range(3) if np.isclose(vals[i], 1.0)]
             clus2LenVecClus[clListInd] = len(vlist)
 
             if clus2LenVecClus[clListInd] == 0:  # If the vector basis is empty, don't consider the cluster
