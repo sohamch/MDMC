5192367cca7941a285ded86c574c56e195a7b5a3
sohamch <ch.soham@gmail.com>
2023-07-07 10:54:27 -0500

    Adding atom count.

--- Utils/MEAM_MC/launch_test_job.sh
+++ Utils/MEAM_MC/launch_test_job.sh
@@ -6,7 +6,7 @@ cd MC_test_traj
 
 mkdir 1
 cd 1
-python3 ../../Init_state_MC.py -pp $potpath -T 1073 -nt 500 -ne 5 -ns 1 -dmp -dpf test_args_first_run.txt 
+python3 ../../Init_state_MC.py -pp $potpath -T 1073 -na 99 100 100 100 100 -nt 500 -ne 5 -ns 1 -dmp -dpf test_args_first_run.txt
 
 # remove the last 20 files to simulate incomplete run
 for i in {480..500}
