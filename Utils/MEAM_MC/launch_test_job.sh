55112ebb3ce6b31a1399d2600914455306964566
sohamch <ch.soham@gmail.com>
2023-07-24 21:58:16 -0500

    using etol for tests.

--- Utils/MEAM_MC/launch_test_job.sh
+++ Utils/MEAM_MC/launch_test_job.sh
@@ -6,7 +6,7 @@ cd MC_test_traj
 
 mkdir 1
 cd 1
-python3 ../../Init_state_MC.py -nosr -pp $potpath -T 1073 -etol 0.0 -ftol 0.001 -na 99 100 100 100 100 -nt 100 -ne 5 -ns 1 -dmp -dpf test_args_first_run.txt
+python3 ../../Init_state_MC.py -nosr -pp $potpath -T 1073 -etol 1e-8 -ftol 0.0 -na 99 100 100 100 100 -nt 100 -ne 5 -ns 1 -dmp -dpf test_args_first_run.txt
 
 # remove the last 20 files to simulate incomplete run
 for i in {80..100}
@@ -20,4 +20,4 @@ done
 cp in.minim in_run_1.minim
 
 # Now re-run from last checkpoint
-python3 ../../Init_state_MC.py -etol 0.0 -ftol 0.001 -nosr -pp $potpath -T 1073 -ckp -nt 100 -ne 5 -ns 1 -dmp -dpf test_args_second_run.txt
+python3 ../../Init_state_MC.py -etol 1e-8 -ftol 0.0 -nosr -pp $potpath -T 1073 -ckp -nt 100 -ne 5 -ns 1 -dmp -dpf test_args_second_run.txt
