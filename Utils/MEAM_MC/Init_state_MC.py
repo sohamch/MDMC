1e47796845bb301e9bfe33897d90ae2e26149152
sohamch <ch.soham@gmail.com>
2023-07-28 15:13:40 -0500

    Minor Update in comments.

--- Utils/MEAM_MC/Init_state_MC.py
+++ Utils/MEAM_MC/Init_state_MC.py
@@ -100,7 +100,7 @@ def MC_Run(T, SwapRun, ASE_Super, elems,
         assert len(rand_steps) == lastChkPt, "{} {}".format(len(rand_steps), lastChkPt)
         assert len(swap_steps) == lastChkPt, "{} {}".format(len(swap_steps), lastChkPt)
 
-        # Then clear the backup folder
+        # Then clear the backup folder and only store the last loaded history
         clear_backup(lastChkPt, Eng_steps_all, accepts, rand_steps, swap_steps)
 
     # write the supercell as a lammps file
