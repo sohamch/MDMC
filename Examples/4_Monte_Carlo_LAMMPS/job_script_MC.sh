a7c97e7e98b087456ba0674f1962d174d9b8bbb6
sohamch <ch.soham@gmail.com>
2023-12-04 05:15:08 -0600

    Updating extraction and example script.

--- Examples/4_Monte_Carlo_LAMMPS/job_script_MC.sh
+++ Examples/4_Monte_Carlo_LAMMPS/job_script_MC.sh
@@ -20,23 +20,27 @@ VKMC=/path/to/the/local/copy/of/the/VKMC/repository # We Need to add the full pa
 potpath=$VKMC/Utils/pot
 
 # Path to the directory containing the Monte Carlo Python module
-$LMP_MC=$VKMC/Utils/MEAM_MC/
+$LMP_MC=$VKMC/Utils/MEAM_MC
 
 
 # Runs from scratch
-# We'll simulate 40 independent trajectories at 1073K, each starting from a random state, and every 250 states gathered from each trajectory.
-# We will initialize 500 atom supercells with one vacancy (default option) and use 99, 100, 100, 100, 100 atoms of Co, Ni, Cr Fe and Mn
-# respectively.
 # By default, the supercells are orthogonal supercells, unless the flag "-pr" or "--Primitive" is set. In our paper, we used orthogonal supercells.
 
 T=1073
+
+# We first specify a "jobID" which specifies the atom count
+# for jobID=1, the atom counts will be 99, 100, 100, 100, 100
+# for jobID = 2, we'll set them as 100, 99, 100, 100, 100 and so on.
 jobID=1 # or 2 or 3 or 4 or 5 depending on which simulation is being done.
-for i in {1..40} # For each jobID, we generate 16 trajectories with specified atom counts.
+mkdir ${T}_${jobID}
+
+# In each such job, We'll simulate 40 independent trajectories at 1073K, each starting from a random state.
+for i in {1..40}
 do
-	mkdir ${T}_${jobID}_${i} # This is the directory in which all run results will be stored.
-	cd ${T}_${jobID}_${i}
+	mkdir ${T}_${jobID}/${T}_${jobID}_${i} # This is the directory in which all run results will be stored.
+	cd ${T}_${jobID}/${T}_${jobID}_${i}
 	python -u $LMP_MC/Init_state_MC.py -pp $potpath -T $T -etol 0.0 -ftol 0.001 -na 99 100 100 100 100 -nt 120000 -ne 250 -ns 250 -dmp -dpf args_continue_${i}.txt > JobOut_${i}.txt 2>&1 &
-	cd ../
+	cd ../../
 done
 wait
 
@@ -44,8 +48,8 @@ wait
 # This will restart the simulations from two supercells before the last saved ones.
 #for i in {1..40}
 #do
-#	cd ${T}_${jobID}_${i}
+#	cd ${T}_${jobID}/${T}_${jobID}_${i}
 #	python -u $LMP_MC/Init_state_MC.py -pp $potpath -T $T -ckp -nt 120000 -ne 250 -ns 250 -dmp -dpf args_continue_${i}.txt > JobOut_${i}.txt 2>&1 &
-#	cd ../
+#	cd ../../
 #done
 #wait
